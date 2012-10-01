3350d088427a3710681d546a41f1117da740978b
Mograine <ghiot.pierre@gmail.com>
2012-10-01 04:43:31 +0200

    Void Storage (3/6)

--- src/server/game/Handlers/VoidStorageHandler.cpp
+++ src/server/game/Handlers/VoidStorageHandler.cpp
@@ -82,23 +82,29 @@ void WorldSession::HandleVoidStorageQuery(WorldPacket& recvData)
     Player* player = GetPlayer();
 
     ObjectGuid npcGuid;
-    npcGuid[4] = recvData.ReadBit();
-    npcGuid[0] = recvData.ReadBit();
-    npcGuid[5] = recvData.ReadBit();
-    npcGuid[7] = recvData.ReadBit();
-    npcGuid[6] = recvData.ReadBit();
-    npcGuid[3] = recvData.ReadBit();
-    npcGuid[1] = recvData.ReadBit();
-    npcGuid[2] = recvData.ReadBit();
+    uint8 unk1;
+    recvData.read_skip<uint8>();
 
-    recvData.ReadByteSeq(npcGuid[5]);
-    recvData.ReadByteSeq(npcGuid[6]);
+    npcGuid[7] = recvData.ReadBit();
+    npcGuid[3] = recvData.ReadBit();
+    unk1 = recvData.ReadBit();
+    npcGuid[6] = recvData.ReadBit();
+    npcGuid[2] = recvData.ReadBit();
+    npcGuid[5] = recvData.ReadBit();
+    npcGuid[4] = recvData.ReadBit();
+    npcGuid[1] = recvData.ReadBit();
+    npcGuid[0] = recvData.ReadBit();
+
+    recvData.FlushBits();
+
+    recvData.ReadByteSeq(npcGuid[4]);
     recvData.ReadByteSeq(npcGuid[3]);
     recvData.ReadByteSeq(npcGuid[7]);
-    recvData.ReadByteSeq(npcGuid[1]);
-    recvData.ReadByteSeq(npcGuid[0]);
-    recvData.ReadByteSeq(npcGuid[4]);
+    recvData.ReadByteSeq(npcGuid[6]);
     recvData.ReadByteSeq(npcGuid[2]);
+    recvData.ReadByteSeq(npcGuid[0]);
+    recvData.ReadByteSeq(npcGuid[1]);
+    recvData.ReadByteSeq(npcGuid[5]);
 
     Creature* unit = player->GetNPCIfCanInteractWith(npcGuid, UNIT_NPC_FLAG_VAULTKEEPER);
     if (!unit)
@@ -133,50 +139,49 @@ void WorldSession::HandleVoidStorageQuery(WorldPacket& recvData)
         ObjectGuid itemId = item->ItemId;
         ObjectGuid creatorGuid = item->CreatorGuid;
 
-        data.WriteBit(creatorGuid[3]);
-        data.WriteBit(itemId[5]);
-        data.WriteBit(creatorGuid[6]);
-        data.WriteBit(creatorGuid[1]);
-        data.WriteBit(itemId[1]);
-        data.WriteBit(itemId[3]);
-        data.WriteBit(itemId[6]);
-        data.WriteBit(creatorGuid[5]);
         data.WriteBit(creatorGuid[2]);
-        data.WriteBit(itemId[2]);
-        data.WriteBit(creatorGuid[4]);
-        data.WriteBit(itemId[0]);
         data.WriteBit(itemId[4]);
-        data.WriteBit(itemId[7]);
-        data.WriteBit(creatorGuid[0]);
+        data.WriteBit(itemId[1]);
         data.WriteBit(creatorGuid[7]);
+        data.WriteBit(itemId[6]);
+        data.WriteBit(creatorGuid[0]);
+        data.WriteBit(itemId[5]);
+        data.WriteBit(itemId[3]);
+        data.WriteBit(creatorGuid[4]);
+        data.WriteBit(creatorGuid[1]);
+        data.WriteBit(itemId[0]);
+        data.WriteBit(itemId[2]);
+        data.WriteBit(creatorGuid[5]);
+        data.WriteBit(itemId[7]);
+        data.WriteBit(creatorGuid[6]);
+        data.WriteBit(creatorGuid[3]);
 
+        itemData.WriteByteSeq(itemId[4]);
+        itemData.WriteByteSeq(itemId[1]);
         itemData.WriteByteSeq(creatorGuid[3]);
+        itemData.WriteByteSeq(itemId[7]);
+        itemData.WriteByteSeq(creatorGuid[0]);
 
-        itemData << uint32(item->ItemSuffixFactor);
-
-        itemData.WriteByteSeq(creatorGuid[4]);
-
+        itemData << uint32(item->ItemEntry);
         itemData << uint32(i);
 
-        itemData.WriteByteSeq(itemId[0]);
-        itemData.WriteByteSeq(itemId[6]);
-        itemData.WriteByteSeq(creatorGuid[0]);
+        itemData.WriteByteSeq(itemId[3]);
+        itemData.WriteByteSeq(creatorGuid[1]);
+        itemData.WriteByteSeq(creatorGuid[4]);
+        itemData.WriteByteSeq(creatorGuid[2]);
 
         itemData << uint32(item->ItemRandomPropertyId);
 
-        itemData.WriteByteSeq(itemId[4]);
-        itemData.WriteByteSeq(itemId[5]);
-        itemData.WriteByteSeq(itemId[2]);
-        itemData.WriteByteSeq(creatorGuid[2]);
-        itemData.WriteByteSeq(creatorGuid[6]);
-        itemData.WriteByteSeq(itemId[1]);
-        itemData.WriteByteSeq(itemId[3]);
         itemData.WriteByteSeq(creatorGuid[5]);
+        itemData.WriteByteSeq(creatorGuid[6]);
         itemData.WriteByteSeq(creatorGuid[7]);
+        itemData.WriteByteSeq(itemId[5]);
+        itemData.WriteByteSeq(itemId[0]);
+        itemData.WriteByteSeq(itemId[6]);
 
-        itemData << uint32(item->ItemEntry);
+        itemData << uint32(item->ItemSuffixFactor);
 
-        itemData.WriteByteSeq(itemId[7]);
+        itemData.WriteByteSeq(itemId[2]);
     }
 
     data.FlushBits();
