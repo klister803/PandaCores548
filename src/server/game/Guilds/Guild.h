53134b6d4787af2b261bf9dae11ba62e69ccfe9f
Selenium <Selenium@millenium-servers.com>
2012-10-29 21:10:23 +0100

    CodeStyle

--- src/server/game/Guilds/Guild.h
+++ src/server/game/Guilds/Guild.h
@@ -184,16 +184,16 @@ enum GuildBankRights
 
 enum GuildBankEventLogTypes
 {
-GUILD_BANK_LOG_DEPOSIT_ITEM         = 1,
-GUILD_BANK_LOG_WITHDRAW_ITEM        = 2,
-GUILD_BANK_LOG_MOVE_ITEM            = 3,
-GUILD_BANK_LOG_DEPOSIT_MONEY        = 4,
-GUILD_BANK_LOG_WITHDRAW_MONEY       = 5,
-GUILD_BANK_LOG_REPAIR_MONEY         = 6,
-GUILD_BANK_LOG_MOVE_ITEM2           = 7,
-GUILD_BANK_LOG_UNK1                 = 8,
-GUILD_BANK_LOG_BUY_SLOT             = 9,
-GUILD_BANK_LOG_CASH_FLOW_DEPOSIT    = 10,
+    GUILD_BANK_LOG_DEPOSIT_ITEM         = 1,
+    GUILD_BANK_LOG_WITHDRAW_ITEM        = 2,
+    GUILD_BANK_LOG_MOVE_ITEM            = 3,
+    GUILD_BANK_LOG_DEPOSIT_MONEY        = 4,
+    GUILD_BANK_LOG_WITHDRAW_MONEY       = 5,
+    GUILD_BANK_LOG_REPAIR_MONEY         = 6,
+    GUILD_BANK_LOG_MOVE_ITEM2           = 7,
+    GUILD_BANK_LOG_UNK1                 = 8,
+    GUILD_BANK_LOG_BUY_SLOT             = 9,
+    GUILD_BANK_LOG_CASH_FLOW_DEPOSIT    = 10,
 };
 
 enum GuildEventLogTypes
