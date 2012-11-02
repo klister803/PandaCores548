3ab23c7d1e67488678b4ba547191c9a583b2ae0b
Selenium <Selenium@millenium-servers.com>
2012-11-02 13:09:11 +0100

    Revert "Fix quest honor reward"
    This reverts commit 4d34e5c3198e085bd999c8d06ad54f7914db1e3d.

--- src/server/game/DataStores/DBCStores.cpp
+++ src/server/game/DataStores/DBCStores.cpp
@@ -228,6 +228,7 @@ DBCStorage <TaxiPathEntry> sTaxiPathStore(TaxiPathEntryfmt);
 TaxiPathNodesByPath sTaxiPathNodesByPath;
 static DBCStorage <TaxiPathNodeEntry> sTaxiPathNodeStore(TaxiPathNodeEntryfmt);
 
+DBCStorage <TeamContributionPointsEntry> sTeamContributionPointsStore(TeamContributionPointsfmt);
 DBCStorage <TotemCategoryEntry> sTotemCategoryStore(TotemCategoryEntryfmt);
 DBCStorage <VehicleEntry> sVehicleStore(VehicleEntryfmt);
 DBCStorage <VehicleSeatEntry> sVehicleSeatStore(VehicleSeatEntryfmt);
@@ -706,6 +707,7 @@ void LoadDBCStores(const std::string& dataPath)
         }
     }
 
+    //LoadDBC(availableDbcLocales, bad_dbc_files, sTeamContributionPointsStore, dbcPath, "TeamContributionPoints.dbc");
     LoadDBC(availableDbcLocales, bad_dbc_files, sTotemCategoryStore,          dbcPath, "TotemCategory.dbc");//14545
 
     LoadDBC(availableDbcLocales, bad_dbc_files, sVehicleStore,                dbcPath, "Vehicle.dbc");//14545
--- src/server/game/Quests/QuestDef.cpp
+++ src/server/game/Quests/QuestDef.cpp
@@ -342,11 +342,10 @@ uint32 Quest::CalculateHonorGain(uint8 level) const
     /*if (GetRewHonorAddition() > 0 || GetRewHonorMultiplier() > 0.0f)
     {
         // values stored from 0.. for 1...
-        TeamContributionPointsEntry const* tc = sTeamContributionPointsStore.LookupEntry(level);
+        TeamContributionPointsEntry const* tc = sTeamContributionPointsStore.LookupEntry(level-1);
         if (!tc)
             return 0;
-
-        honor = uint32(tc->value * GetRewHonorMultiplier() * 0.1f);
+        honor = uint32(tc->value * GetRewHonorMultiplier() * 0.1000000014901161);
         honor += GetRewHonorAddition();
     }*/
 
