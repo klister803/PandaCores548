4a659437860afc30359ffc16d0774aa01988b271
Selenium <Selenium@millenium-servers.com>
2012-11-05 17:50:05 +0100

    Packet/IO (2)

--- src/server/game/Server/Protocol/Opcodes.h
+++ src/server/game/Server/Protocol/Opcodes.h
@@ -281,6 +281,7 @@ enum Opcodes
     CMSG_IGNORE_TRADE                                 = 0,      // 5.0.5 16048 Deprecated ?
     CMSG_INITIATE_TRADE                               = 0x6F1,  // 5.0.5 16048
     CMSG_INSPECT                                      = 0x254,  // 5.0.5 16048
+    CMSG_INSPECT_HONOR_STATS                          = 0,      // 0x791E in 4.3.4 FIND ME, I AM WALDO !
     CMSG_INSTANCE_LOCK_WARNING_RESPONSE               = 0x816,  // 5.0.5 16048
     CMSG_ITEM_REFUND                                  = 0x120,  // 5.0.5 16048
     CMSG_ITEM_REFUND_INFO                             = 0x355,  // 5.0.5 16048
@@ -1128,7 +1129,7 @@ enum Opcodes
     SMSG_PLAY_OBJECT_SOUND                            = 0xCED,  // 5.0.5 16048
     SMSG_PLAY_SOUND                                   = 0x873,  // 5.0.5 16048
 	SMSG_PLAY_ONE_SHOT_ANIM_KIT						  = 0x000,  // 5.0.5 16048
-    SMSG_PLAY_SPELL_IMPACT                            = 0x000,  // 5.0.5 16048
+    //SMSG_PLAY_SPELL_IMPACT                            = 0x000,  // 5.0.5 16048 removed in commit c041875
     SMSG_PLAY_SPELL_VISUAL                            = 0xAE4,  // 5.0.5 16048
     SMSG_PLAY_SPELL_VISUAL_KIT                        = 0xAE4,  // 5.0.5 16048
     SMSG_PLAY_TIME_WARNING                            = 0x817,  // 5.0.5 16048
@@ -1306,8 +1307,9 @@ enum Opcodes
     SMSG_TOTEM_CREATED                                = 0x918,  // 5.0.5 16048
     SMSG_TRADE_STATUS                                 = 0xF85,  // 5.0.5 16048
     SMSG_TRADE_STATUS_EXTENDED                        = 0xAFB,  // 5.0.5 16048
-    SMSG_TRAINER_BUY_SUCCEEDED                        = 0xD31,  // 5.0.5 16048
+    //SMSG_TRAINER_BUY_SUCCEEDED                        = 0xD31,  // 5.0.5 16048 removed in commit c041875
     SMSG_TRAINER_LIST                                 = 0xC65,  // 5.0.5 16048
+    SMSG_TRAINER_SERVICE                              = 0,      // 0x6A05 in 4.3.4 FIND ME I AM WALDO !
     SMSG_TRANSFER_ABORTED                             = 0x889,  // 5.0.5 16048
     SMSG_TRANSFER_PENDING                             = 0xF79,  // 5.0.5 16048
     SMSG_TRIGGER_CINEMATIC                            = 0xC18,  // 5.0.5 16048
