6c9ab250a40b1b8eeb3c49c8f1fec2811a5e7025
Morgoporc <j.fraisse45@laposte.net>
2012-11-19 22:35:00 +0100

    Fix Jab damages

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -556,14 +556,7 @@ void Spell::EffectSchoolDMG(SpellEffIndex effIndex)
                     case 100780: // Jab
                     {
                         if (m_caster->GetTypeId() == TYPEID_PLAYER)
-                        {
-                            float ap = m_caster->GetTotalAttackPowerValue(BASE_ATTACK);
-                            float mwb = m_caster->GetWeaponDamageRange(BASE_ATTACK, MINDAMAGE);
-                            float MWB = m_caster->GetWeaponDamageRange(BASE_ATTACK, MAXDAMAGE);
-                            float MWS = m_caster->GetAttackTime(BASE_ATTACK);
-                            float mws = MWS;
-                            damage += irand(int32((1.5f * 0.898882f * (mwb / (MWS / 1000.0f)) + 1 * (mwb / 2.0f / (mws / 1000.0f)) + (ap / 14.0f) - 1.0f)), int32((1.5f * 0.898882f * (MWB / (MWS / 1000.0f)) + 1 * (MWB / 2.0f / (mws / 1000.0f)) + (ap / 14.0f) + 1)));
-                        }
+                            damage = CalculateMonkMeleeAttacks(m_caster, 1.5f, 14);
                         break;
                     }
                     case 115080: // Touch of Death
