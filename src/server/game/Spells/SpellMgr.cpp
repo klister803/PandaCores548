cb9c9cb2528f8401189f5decba013041831cb22b
Morgoporc <j.fraisse45@laposte.net>
2012-12-03 01:53:10 +0100

    Fix Fear Ward take only one charge now

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3077,6 +3077,9 @@ void SpellMgr::LoadSpellCustomAttr()
                 spellInfo->AttributesCu |= SPELL_ATTR0_CU_NEGATIVE_EFF0;
                 break;
             // Custom MoP Script
+            case 6346: // Fear Ward
+                spellInfo->ProcCharges = 1;
+                break;
             case 8122: // Psychic Scream
                 spellInfo->Effects[2].ApplyAuraName = SPELL_AURA_MOD_FEAR;
                 spellInfo->MaxAffectedTargets = 5;
