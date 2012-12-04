873a3381c863453cf0b88f785059397b2f85ecd6
Mograine <ghiot.pierre@gmail.com>
2012-12-04 18:34:53 +0100

    Correction de /kick et /ban sur les channels

--- src/server/shared/Common.h
+++ src/server/shared/Common.h
@@ -149,11 +149,14 @@ enum TimeConstants
 
 enum AccountTypes
 {
-    SEC_PLAYER         = 0,
-    SEC_MODERATOR      = 1,
-    SEC_GAMEMASTER     = 2,
-    SEC_ADMINISTRATOR  = 3,
-    SEC_CONSOLE        = 4                                  // must be always last in list, accounts must have less security level always also
+    SEC_PLAYER                = 0,
+    SEC_MODERATOR             = 1,
+    SEC_GAMEMASTER            = 2,
+    SEC_CONFIRMED_GAMEMASTER  = 3,
+    SEC_REALM_LEADER          = 4,
+    SEC_GM_LEADER             = 5,
+    SEC_ADMINISTRATOR         = 6,
+    SEC_CONSOLE               = 7                                  // must be always last in list, accounts must have less security level always also
 };
 
 enum LocaleConstant
