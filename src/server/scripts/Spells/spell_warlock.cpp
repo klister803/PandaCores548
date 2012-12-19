9fe551ffab3200d6c318a6a69f6927cbb64ba53f
Selenium <Selenium@millenium-servers.com>
2012-12-19 23:56:27 +0100

    Fix Ember Tap

--- src/server/scripts/Spells/spell_warlock.cpp
+++ src/server/scripts/Spells/spell_warlock.cpp
@@ -61,6 +61,49 @@ enum WarlockSpells
     WARLOCK_FIRE_AND_BRIMSTONE              = 108683,
 };
 
+// Ember Tap - 114635
+class spell_warl_ember_tap : public SpellScriptLoader
+{
+    public:
+        spell_warl_ember_tap() : SpellScriptLoader("spell_warl_ember_tap") { }
+
+        class spell_warl_ember_tap_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_warl_ember_tap_SpellScript);
+
+            void HandleOnHit()
+            {
+                if (Player* _player = GetCaster()->ToPlayer())
+                {
+                    if (Unit* target = GetHitUnit())
+                    {
+                        int32 healAmount;
+                        int32 pct;
+                        float Mastery;
+
+                        Mastery = 3.0f * _player->GetFloatValue(PLAYER_MASTERY) / 100.0f;
+
+                        pct = int32(0.15f * (1 + Mastery));
+
+                        healAmount = int32(_player->GetMaxHealth() * pct);
+
+                        SetHitHeal(healAmount);
+                    }
+                }
+            }
+
+            void Register()
+            {
+                OnHit += SpellHitFn(spell_warl_ember_tap_SpellScript::HandleOnHit);
+            }
+        };
+
+        SpellScript* GetSpellScript() const
+        {
+            return new spell_warl_ember_tap_SpellScript();
+        }
+};
+
 // Called By : Incinerate (Fire and Brimstone) - 114654, Conflagrate (Fire and Brimstone) - 108685
 // Curse of the Elements (Fire and Brimstone) - 104225, Curse of Enfeeblement (Fire and Brimstone) - 109468
 // Immolate (Fire and Brimstone) - 108686
@@ -74,11 +117,12 @@ class spell_warl_fire_and_brimstone : public SpellScriptLoader
         {
             PrepareSpellScript(spell_warl_fire_and_brimstone_SpellScript);
 
-            void HandleAfterCast()
+            void HandleOnHit()
             {
                 if (Player* _player = GetCaster()->ToPlayer())
                     if (Unit* target = GetHitUnit())
-                        _player->RemoveAura(WARLOCK_FIRE_AND_BRIMSTONE);
+                        if (_player->HasAura(WARLOCK_FIRE_AND_BRIMSTONE))
+                            _player->RemoveAura(WARLOCK_FIRE_AND_BRIMSTONE);
             }
 
             void Register()
@@ -1017,6 +1061,7 @@ public:
 
 void AddSC_warlock_spell_scripts()
 {
+    new spell_warl_ember_tap();
     new spell_warl_fire_and_brimstone();
     new spell_warl_conflagrate_aura();
     new spell_warl_shadowburn();
