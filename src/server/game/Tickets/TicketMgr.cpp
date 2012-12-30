ce40082430c868de20763c29b544c97f09f4821d
Mograine <ghiot.pierre@gmail.com>
2012-12-30 03:50:36 +0100

    Namelink dans les tickets onlinelist

--- src/server/game/Tickets/TicketMgr.cpp
+++ src/server/game/Tickets/TicketMgr.cpp
@@ -178,8 +178,10 @@ std::string GmTicket::FormatMessageString(ChatHandler& handler, bool detailed) c
     time_t curTime = time(NULL);
 
     std::stringstream ss;
+    std::string nameLink = handler.playerLink(_playerName);
+
     ss << handler.PGetParseString(LANG_COMMAND_TICKETLISTGUID, _id);
-    ss << handler.PGetParseString(LANG_COMMAND_TICKETLISTNAME, _playerName.c_str());
+    ss << handler.PGetParseString(LANG_COMMAND_TICKETLISTNAME, nameLink.c_str());
     ss << handler.PGetParseString(LANG_COMMAND_TICKETLISTAGECREATE, (secsToTimeString(curTime - _createTime, true, false)).c_str());
     ss << handler.PGetParseString(LANG_COMMAND_TICKETLISTAGE, (secsToTimeString(curTime - _lastModifiedTime, true, false)).c_str());
 
