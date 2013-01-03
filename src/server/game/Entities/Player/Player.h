4057965f66672d5a861d0dcaf4e5c7744034020b
Mograine <ghiot.pierre@gmail.com>
2013-01-03 18:18:32 +0100

    Implant Transferts

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -524,16 +524,17 @@ enum PlayerExtraFlags
 // 2^n values
 enum AtLoginFlags
 {
-    AT_LOGIN_NONE              = 0x000,
-    AT_LOGIN_RENAME            = 0x001,
-    AT_LOGIN_RESET_SPELLS      = 0x002,
-    AT_LOGIN_RESET_TALENTS     = 0x004,
-    AT_LOGIN_CUSTOMIZE         = 0x008,
-    AT_LOGIN_RESET_PET_TALENTS = 0x010,
-    AT_LOGIN_FIRST             = 0x020,
-    AT_LOGIN_CHANGE_FACTION    = 0x040,
-    AT_LOGIN_CHANGE_RACE       = 0x080,
-    AT_LOGIN_UNLOCK            = 0x100
+    AT_LOGIN_NONE                   = 0x000,
+    AT_LOGIN_RENAME                 = 0x001,
+    AT_LOGIN_RESET_SPELLS           = 0x002,
+    AT_LOGIN_RESET_TALENTS          = 0x004,
+    AT_LOGIN_CUSTOMIZE              = 0x008,
+    AT_LOGIN_RESET_PET_TALENTS      = 0x010,
+    AT_LOGIN_FIRST                  = 0x020,
+    AT_LOGIN_CHANGE_FACTION         = 0x040,
+    AT_LOGIN_CHANGE_RACE            = 0x080,
+    AT_LOGIN_UNLOCK                 = 0x100,
+    AT_LOGIN_LOCKED_FOR_TRANSFER    = 0x200
 };
 
 typedef std::map<uint32, QuestStatusData> QuestStatusMap;
