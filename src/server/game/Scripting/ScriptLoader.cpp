4c9bc56f7055fe75e724e34078a87bcc26c10320
Naphtaline <le.tux.tueur@gmail.com>
2013-03-30 19:47:30 +0100

    Fix typo (antoine : gros sac à foutre)
    Signed-off-by: Naphtaline <le.tux.tueur@gmail.com>

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -618,7 +618,7 @@ void AddSC_boss_commander_rimok();
 void AddSC_boss_raigonn();
 void AddSC_boss_sha_of_anger();
 void AddSC_boss_galion();
-void AddSC_boss_stone_guard();
+//void AddSC_boss_stone_guard();
 void AddSC_instance_mogu_shan_palace();         // Mogu'Shan Palace
 void AddSC_boss_trial_of_the_king();
 void AddSC_boss_gekkan();
@@ -1295,7 +1295,7 @@ void AddPandarieScripts()
     AddSC_boss_raigonn();
     AddSC_boss_sha_of_anger();                   //Pandaria World Bosses
     AddSC_boss_galion();
-    AddSC_boss_stone_guard();
+    //AddSC_boss_stone_guard();
     AddSC_instance_shadopan_monastery();          // Shadopan Monastery
     AddSC_shadopan_monastery();
     AddSC_boss_gu_cloudstrike();
