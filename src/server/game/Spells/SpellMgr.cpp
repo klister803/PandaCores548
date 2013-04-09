1d2a8125343e8732084178d2093b1e6e1bc37ff9
Selenium <Selenium@millenium-servers.com>
2013-04-09 13:30:02 +0200

    Add opening spell to blood elf monk
    Signed-off-by: Selenium <Selenium@millenium-servers.com>

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -2710,9 +2710,23 @@ void SpellMgr::LoadSpellClassInfo()
         if (ClassID == CLASS_DRUID)
             mSpellClassInfo[ClassID].insert(112857);
         
-        //Sinister Strike Enabler
+        // Sinister Strike Enabler
         if (ClassID == CLASS_ROGUE)
             mSpellClassInfo[ClassID].insert(79327);
+
+        // Opening gameobject
+        if (ClassID == CLASS_MONK)
+        {
+            mSpellClassInfo[ClassID].insert(3365);
+            mSpellClassInfo[ClassID].insert(6247);
+            mSpellClassInfo[ClassID].insert(6477);
+            mSpellClassInfo[ClassID].insert(6478);
+            mSpellClassInfo[ClassID].insert(21651);
+            mSpellClassInfo[ClassID].insert(22810);
+            mSpellClassInfo[ClassID].insert(61437);
+            mSpellClassInfo[ClassID].insert(68398);
+            mSpellClassInfo[ClassID].insert(96220);
+        }
 
         for (uint32 i = 0; i < sSkillLineAbilityStore.GetNumRows(); ++i)
         {
