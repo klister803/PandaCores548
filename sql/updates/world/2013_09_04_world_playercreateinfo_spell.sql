04f1a76adb56636bdbebe7e20e7ec2cf099cade1
Selenium <Selenium@millenium-servers.com>
2013-04-09 13:28:06 +0200

    Typo
    Signed-off-by: Selenium <Selenium@millenium-servers.com>

--- sql/updates/world/2013_09_04_world_playercreateinfo_spell.sql
+++ sql/updates/world/2013_09_04_world_playercreateinfo_spell.sql
@@ -1,9 +1,9 @@
-INSERT INTO `505_world`.`playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','3365','Opening');
-INSERT INTO `505_world`.`playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','6247','Opening'); 
-INSERT INTO `505_world`.`playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','6477','Opening'); 
-INSERT INTO `505_world`.`playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','6478','Opening'); 
-INSERT INTO `505_world`.`playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','21651','Opening'); 
-INSERT INTO `505_world`.`playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','22810','Opening no text'); 
-INSERT INTO `505_world`.`playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','61437','Opening'); 
-INSERT INTO `505_world`.`playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','68398','Opening'); 
-INSERT INTO `505_world`.`playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','96220','Opening');
+INSERT INTO `playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','3365','Opening');
+INSERT INTO `playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','6247','Opening'); 
+INSERT INTO `playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','6477','Opening'); 
+INSERT INTO `playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','6478','Opening'); 
+INSERT INTO `playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','21651','Opening'); 
+INSERT INTO `playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','22810','Opening no text'); 
+INSERT INTO `playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','61437','Opening'); 
+INSERT INTO `playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','68398','Opening'); 
+INSERT INTO `playercreateinfo_spell`(`race`,`class`,`Spell`,`Note`)VALUES('10','10','96220','Opening');
