c8c7a0b786f9c55e286a73aa56ea24e7279ea699
Naphtaline <le.tux.tueur@gmail.com>
2013-04-11 10:13:47 +0200

    disable talent pala - clemency - pending debug SPELL_AURA_411
    Signed-off-by: Naphtaline <le.tux.tueur@gmail.com>

---
+++ sql/updates/world/2013_04_11_world_disables_clemency.sql
@@ -0,0 +1,8 @@
+/*
+SQLyog Community v11.01 (64 bit)
+MySQL - 5.5.24-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) values('0','105622','1','','','Clemency : En attente de debug');
