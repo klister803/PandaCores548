9327b7a16d5d6619c392aa3f805dbde0a41e456c
Morgoporc <j.fraisse45@laposte.net>
2013-04-12 14:28:24 +0200

    Update in channels by Russians
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/game/Chat/Channels/Channel.cpp
+++ src/server/game/Chat/Channels/Channel.cpp
@@ -25,7 +25,7 @@
 #include "AccountMgr.h"
 
 Channel::Channel(const std::string& name, uint32 channel_id, uint32 Team)
- : m_announce(true), m_ownership(true), m_name(name), m_password(""), m_flags(0), m_channelId(channel_id), m_ownerGUID(0), m_Team(Team)
+ : m_announce(true), m_ownership(true), m_name(name), m_password(""), m_flags(0), m_channelId(channel_id), m_ownerGUID(0), m_Team(Team), _special(false)
 {
     m_IsSaved = false;
 
@@ -50,6 +50,11 @@ Channel::Channel(const std::string& name, uint32 channel_id, uint32 Team)
             m_flags |= CHANNEL_FLAG_LFG;
         else                                                // for all other channels
             m_flags |= CHANNEL_FLAG_NOT_LFG;
+    }
+    else if (!stricmp(m_name.c_str(), "world"))
+    {
+        _announce = false;
+        _special = true;
     }
     else                                                    // it's custom channel
     {
