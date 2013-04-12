8aa7010616df6ec140f322c9035cb24deb5e1638
Jeyel <joffrey.lacombe@voila.fr>
2013-04-12 02:04:05 +0200

    Adjust Careful Aim
    Signed-off-by: Jeyel <joffrey.lacombe@voila.fr>

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10999,7 +10999,7 @@ bool Unit::isSpellCrit(Unit* victim, SpellInfo const* spellProto, SpellSchoolMas
                     case 82928: // Aimed Shot (Master Marksman)
                     case 56641: // Steady Shot
                         if (HasAura(34483)) // Careful Aim
-                            if (victim->GetHealthPct() > 80.0f)
+                            if (victim->GetHealthPct() > 90.0f)
                                 crit_chance += 75.0f;
                         break;
                     default:
