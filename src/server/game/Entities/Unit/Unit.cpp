1130e3657bd36da19d25965c855fc05a6d202dfd
Morgoporc <j.fraisse45@laposte.net>
2013-04-13 15:55:35 +0200

    Fix all non melee spells with AP coefficient
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10608,7 +10608,7 @@ uint32 Unit::SpellDamageBonusDone(Unit* victim, SpellInfo const* spellProto, uin
                 WeaponAttackType attType = (spellProto->IsRangedWeaponSpell() && spellProto->DmgClass != SPELL_DAMAGE_CLASS_MELEE) ? RANGED_ATTACK : BASE_ATTACK;
                 float APbonus = float(victim->GetTotalAuraModifier(attType == BASE_ATTACK ? SPELL_AURA_MELEE_ATTACK_POWER_ATTACKER_BONUS : SPELL_AURA_RANGED_ATTACK_POWER_ATTACKER_BONUS));
                 APbonus += GetTotalAttackPowerValue(attType);
-                if (spellProto && (spellProto->Id == 81297 || spellProto->Id == 879 || spellProto->Id == 49184 || spellProto->Id == 45477))
+                if (spellProto && spellProto->DmgClass == SPELL_DAMAGE_CLASS_MAGIC)
                     APbonus += GetTotalAttackPowerValue(BASE_ATTACK);
                 DoneTotal += int32(bonus->ap_bonus * stack * ApCoeffMod * APbonus);
             }
