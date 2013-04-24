46caab73417ff352cba021fe45ab110600bc5aae
Morgoporc <j.fraisse45@laposte.net>
2013-04-24 12:07:18 +0200

    Fix Pandemic
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -2733,8 +2733,28 @@ SpellMissInfo Spell::DoSpellHitOnUnit(Unit* unit, uint32 effectMask, bool scaleA
 
                     if (duration != m_spellAura->GetMaxDuration())
                     {
-                        m_spellAura->SetMaxDuration(duration);
-                        m_spellAura->SetDuration(duration);
+                        bool periodicDamage = false;
+                        for (uint8 i = 0; i < MAX_SPELL_EFFECTS; ++i)
+                            if (m_spellAura->GetEffect(i))
+                                if (m_spellAura->GetEffect(i)->GetAuraType() == SPELL_AURA_PERIODIC_DAMAGE)
+                                    periodicDamage = true;
+
+                        // Fix Pandemic
+                        if (periodicDamage && refresh && m_originalCaster->HasAura(131973))
+                        {
+                            int32 newDuration = (duration + m_spellAura->GetDuration()) <= (int32(m_spellAura->GetMaxDuration() * 1.5f)) ?
+                                duration + m_spellAura->GetDuration() : int32(m_spellAura->GetMaxDuration() * 1.5f);
+                            int32 newMaxDuration = (duration + m_spellAura->GetMaxDuration()) <= (int32(m_spellAura->GetMaxDuration() * 1.5f)) ?
+                                duration + m_spellAura->GetMaxDuration() : int32(m_spellAura->GetMaxDuration() * 1.5f);
+
+                            m_spellAura->SetMaxDuration(newMaxDuration);
+                            m_spellAura->SetDuration(newDuration);
+                        }
+                        else
+                        {
+                            m_spellAura->SetMaxDuration(duration);
+                            m_spellAura->SetDuration(duration);
+                        }
                     }
                     m_spellAura->_RegisterForTargets();
                 }
