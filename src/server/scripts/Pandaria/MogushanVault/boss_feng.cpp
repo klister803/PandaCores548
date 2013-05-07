ce4c61aaf09cff72031eba83d311b94127b43981
Mograine <ghiot.pierre@gmail.com>
2013-05-07 10:27:14 +0200

    Typo compil

--- src/server/scripts/Pandaria/MogushanVault/boss_feng.cpp
+++ src/server/scripts/Pandaria/MogushanVault/boss_feng.cpp
@@ -66,11 +66,11 @@ enum ePhases
 class boss_feng : public CreatureScript
 {
     public:
-        boss_generic_guardian() : CreatureScript("boss_generic_guardian") {}
+        boss_feng() : CreatureScript("boss_feng") {}
 
-        struct boss_generic_guardianAI : public BossAI
+        struct boss_fengAI : public BossAI
         {
-            boss_generic_guardianAI(Creature* creature) : BossAI(creature, DATA_FENG), summons(creature)
+            boss_fengAI(Creature* creature) : BossAI(creature, DATA_FENG), summons(creature)
             {
                 pInstance = creature->GetInstanceScript();
             }
@@ -105,93 +105,6 @@ class boss_feng : public CreatureScript
 
                 switch(events.ExecuteEvent())
                 {
-                    case EVENT_CHECK_NEAR_GUARDIANS:
-                    {
-                        bool hasNearGardian = CheckNearGuardians();
-
-                        if (!isInTrueForm && hasNearGardian)
-                        {
-                            me->RemoveAurasDueToSpell(SPELL_SOLID_STONE);
-                            me->CastSpell(me, spellTrueFormId, true);
-                            isInTrueForm = true;
-                        }
-                        else if (isInTrueForm && !hasNearGardian)
-                        {
-                            me->CastSpell(me, SPELL_SOLID_STONE, true);
-                            me->RemoveAurasDueToSpell(spellTrueFormId);
-                            isInTrueForm = false;
-                        }
-
-                        events.ScheduleEvent(EVENT_CHECK_NEAR_GUARDIANS, 2000);
-                        break;
-                    }
-                    case EVENT_CHECK_ENERGY:
-                        if (me->GetPower(POWER_ENERGY) >= 100)
-                        {
-                            me->CastSpell(me, spellOverloadId, false);
-                            me->RemoveAurasDueToSpell(spellPetrificationId);
-                            pInstance->DoRemoveAurasDueToSpellOnPlayers(spellPetrificationBarId);
-                        }
-
-                        events.ScheduleEvent(EVENT_CHECK_ENERGY, 1000);
-                        break;
-                    case EVENT_REND_FLESH:
-                        if (Unit* victim = SelectTarget(SELECT_TARGET_TOPAGGRO))
-                            me->CastSpell(victim, SPELL_REND_FLESH, false);
-
-                        events.ScheduleEvent(EVENT_REND_FLESH, 20000);
-                        break;
-                    case EVENT_MAIN_ATTACK:
-                        if (isInTrueForm)
-                        {
-                            switch (me->GetEntry())
-                            {
-                                case NPC_JADE:
-                                {
-                                    if (Unit* victim = SelectTarget(SELECT_TARGET_TOPAGGRO))
-                                        me->CastSpell(victim, spellMainAttack, false);
-                                    break;
-                                }
-                                case NPC_COBALT:
-                                case NPC_AMETHYST:
-                                {
-                                    if (Unit* victim = SelectTarget(SELECT_TARGET_RANDOM))
-                                        me->CastSpell(victim, spellMainAttack, false);
-                                    break;
-                                }
-                                case NPC_JASPER:
-                                {
-                                    std::list<Player*> playerList;
-                                    std::list<Player*> tempPlayerList;
-                                    GetPlayerListInGrid(playerList, me, 100.0f);
-
-                                    for (auto player: playerList)
-                                        if (player->isAlive() && !player->HasAura(SPELL_JASPER_CHAINS))
-                                            tempPlayerList.push_back(player);
-
-                                    if (tempPlayerList.size() < 2)
-                                        break;
-
-                                    JadeCore::Containers::RandomResizeList(tempPlayerList, 2);
-                                    
-                                    Player* firstPlayer  = *tempPlayerList.begin();
-                                    Player* SecondPlayer = *(++(tempPlayerList.begin()));
-
-                                    if (!firstPlayer || !SecondPlayer)
-                                        break;
-
-                                    if (AuraPtr aura = me->AddAura(SPELL_JASPER_CHAINS, firstPlayer))
-                                        aura->SetScriptGuid(0, SecondPlayer->GetGUID());
-
-                                    if (AuraPtr aura = me->AddAura(SPELL_JASPER_CHAINS, SecondPlayer))
-                                        aura->SetScriptGuid(0, firstPlayer->GetGUID());
-                                    break;
-                                }
-                            }
-                        }
-
-                        events.ScheduleEvent(EVENT_MAIN_ATTACK, 17500);
-                        break;
                     default:
                         break;
                 }
@@ -202,194 +115,11 @@ class boss_feng : public CreatureScript
 
         CreatureAI* GetAI(Creature* creature) const
         {
-            return new boss_generic_guardianAI(creature);
+            return new boss_fengAI(creature);
         }
 };
 
-enum eMineSpell
+void AddSC_boss_feng()
 {
-    SPELL_COBALT_MINE_VISUAL    = 129455,
-    SPELL_COBALT_MINE_EXPLOSION = 116281
-};
-
-class mob_cobalt_mine : public CreatureScript
-{
-    public:
-        mob_cobalt_mine() : CreatureScript("mob_cobalt_mine") {}
-
-        struct mob_cobalt_mineAI : public ScriptedAI
-        {
-            mob_cobalt_mineAI(Creature* creature) : ScriptedAI(creature)
-            {}
-
-            uint32 preparationTimer;
-            bool isReady;
-
-            void Reset()
-            {
-                preparationTimer = 3000;
-                isReady = false;
-                me->AddAura(SPELL_COBALT_MINE_VISUAL, me);
-            }
-
-            void UpdateAI(const uint32 diff)
-            {
-                if (preparationTimer)
-                {
-                    if (preparationTimer <= diff)
-                    {
-                        isReady = true;
-                        preparationTimer = 0;
-                    }
-                    else
-                        preparationTimer -= diff;
-                }
-
-                if (isReady)
-                    if (me->SelectNearestPlayerNotGM(6.5f))
-                    {
-                        me->CastSpell(me, SPELL_COBALT_MINE_EXPLOSION, false);
-                        me->DespawnOrUnsummon();
-                    }
-            }
-        };
-
-        CreatureAI* GetAI(Creature* creature) const
-        {
-            return new mob_cobalt_mineAI(creature);
-        }
-};
-
-class spell_petrification : public SpellScriptLoader
-{
-    public:
-        spell_petrification() : SpellScriptLoader("spell_petrification") { }
-
-        class spell_petrification_AuraScript : public AuraScript
-        {
-            PrepareAuraScript(spell_petrification_AuraScript);
-
-            void HandleTriggerSpell(constAuraEffectPtr aurEff)
-            {
-                PreventDefaultAction();
-
-                Unit* caster = GetCaster();
-
-                if (!caster)
-                    return;
-
-                std::list<Player*> playerList;
-                GetPlayerListInGrid(playerList, caster, 200.0f);
-
-                for (auto target: playerList)
-                {
-                    if (target->HasAura(SPELL_TOTALY_PETRIFIED))
-                        continue;
-
-                    uint32 triggeredSpell = GetSpellInfo()->Effects[0].TriggerSpell;
-
-                    if (!target->HasAura(triggeredSpell))
-                        caster->AddAura(triggeredSpell, target);
-
-                    if (AuraPtr triggeredAura = target->GetAura(triggeredSpell))
-                    {
-                        uint8 stackCount = triggeredAura->GetStackAmount();
-
-                        uint8 newStackCount = stackCount + 5 > 100 ? 100: stackCount + 5;
-                        triggeredAura->SetStackAmount(newStackCount);
-                        triggeredAura->RefreshDuration();
-                        triggeredAura->RecalculateAmountOfEffects();
-
-                        target->SetPower(POWER_ALTERNATE_POWER, newStackCount);
-
-                        if (newStackCount >= 100)
-                            caster->AddAura(SPELL_TOTALY_PETRIFIED, target);
-                    }
-                }
-            }
-
-            void Register()
-            {
-                OnEffectPeriodic += AuraEffectPeriodicFn(spell_petrification_AuraScript::HandleTriggerSpell, EFFECT_0, SPELL_AURA_PERIODIC_TRIGGER_SPELL);
-            }
-        };
-
-        AuraScript* GetAuraScript() const
-        {
-            return new spell_petrification_AuraScript();
-        }
-};
-
-class spell_jasper_chains : public SpellScriptLoader
-{
-    public:
-        spell_jasper_chains() : SpellScriptLoader("spell_jasper_chains") { }
-
-        class spell_jasper_chains_AuraScript : public AuraScript
-        {
-            PrepareAuraScript(spell_jasper_chains_AuraScript);
-            uint64 playerLinkedGuid;
-
-            bool Load()
-            {
-                playerLinkedGuid = 0;
-                return true;
-            }
-
-            void SetGuid(uint32 type, uint64 guid)
-            {
-                playerLinkedGuid = guid;
-            }
-
-            void HandlePeriodic(constAuraEffectPtr aurEff)
-            {
-                Unit* caster = GetCaster();
-                Unit* target = GetTarget();
-                const SpellInfo* spell = GetSpellInfo();
-                Player* linkedPlayer = sObjectAccessor->GetPlayer(*target, playerLinkedGuid);
-
-                if (!caster || !target || !spell || !linkedPlayer || !linkedPlayer->isAlive() || !linkedPlayer->HasAura(spell->Id))
-                    if (AuraPtr myaura = GetAura())
-                    {
-                        myaura->Remove();
-                        return;
-                    }
-
-                if (target->GetDistance(linkedPlayer) > spell->Effects[EFFECT_0].BasePoints)
-                {
-                    if (AuraPtr aura = target->GetAura(spell->Id))
-                    {
-                        if (aura->GetStackAmount() >= 15)
-                        {
-                            aura->Remove();
-                            return;
-                        }
-                    }
-                    
-                    caster->AddAura(spell->Id, target);
-                    target->CastSpell(linkedPlayer, SPELL_JASPER_CHAINS_DAMAGE, true);
-                }
-                else
-                    target->CastSpell(linkedPlayer, SPELL_JASPER_CHAINS_VISUAL, true);
-            }
-
-            void Register()
-            {
-                OnEffectPeriodic += AuraEffectPeriodicFn(spell_jasper_chains_AuraScript::HandlePeriodic, EFFECT_0, SPELL_AURA_PERIODIC_DUMMY);
-            }
-        };
-
-        AuraScript* GetAuraScript() const
-        {
-            return new spell_jasper_chains_AuraScript();
-        }
-};
-
-void AddSC_boss_stone_guard()
-{
-    new boss_stone_guard_controler();
-    new boss_generic_guardian();
-    new mob_cobalt_mine();
-    new spell_petrification();
-    new spell_jasper_chains();
+    new boss_feng();
 }
