4827ec1e8e6872198908edc5126f0381c4e4f5aa
Naphtaline <le.tux.tueur@gmail.com>
2013-05-07 18:05:46 +0200

    mogu shan vaulth enormous stone quilen
    Signed-off-by: Naphtaline <le.tux.tueur@gmail.com>

--- src/server/scripts/Pandaria/MogushanVault/mogu_shan_vault.cpp
+++ src/server/scripts/Pandaria/MogushanVault/mogu_shan_vault.cpp
@@ -24,6 +24,10 @@ enum spells
 {
     SPIRIT_BOLT = 121224,
     GROUND_SLAM = 121087,
+    PETRIFICATION = 125090,
+    PETRIFIED = 125092,
+    FULLY_PETRIFIED = 115877,
+    MONSTROUS_BITE = 125096,
 };
 
 class mob_cursed_mogu_sculture : public CreatureScript
@@ -94,7 +98,132 @@ class mob_cursed_mogu_sculture : public CreatureScript
         }
 };
 
+class mob_enormous_stone_quilen : public CreatureScript
+{
+    public:
+        mob_enormous_stone_quilen() : CreatureScript("mob_enormous_stone_quilen") {}
+
+        struct mob_enormous_stone_quilenAI : public ScriptedAI
+        {
+            mob_enormous_stone_quilenAI(Creature* creature) : ScriptedAI(creature)
+            {
+                pInstance = creature->GetInstanceScript();
+            }
+
+            InstanceScript* pInstance;
+            EventMap events;
+            uint32 monstrousBiteTimer;
+
+            void Reset()
+            {
+                monstrousBiteTimer = urand (3000, 5000);
+            }
+
+            void EnterCombat(Unit* attacker)
+            {
+                me->AddAura(PETRIFICATION, me);
+            }
+
+            void UpdateAI(const uint32 diff)
+            {
+                if (!UpdateVictim())
+                    return;
+
+                if (monstrousBiteTimer <= diff)
+                {
+                    if (Unit* target = me->SelectNearestTarget(5.0f))
+                    {
+                        if (!target->IsFriendlyTo(me))
+                            me->CastSpell(target, MONSTROUS_BITE, true);
+                    }
+                    monstrousBiteTimer = urand(6000, 8000);
+                }
+                else
+                    monstrousBiteTimer -= diff;
+
+                DoMeleeAttackIfReady();
+            }
+        };
+
+        CreatureAI* GetAI(Creature* creature) const
+        {
+            return new mob_enormous_stone_quilenAI(creature);
+        }
+};
+
+class spell_mogu_petrification : public SpellScriptLoader
+{
+    public:
+        spell_mogu_petrification() : SpellScriptLoader("spell_mogu_petrification") { }
+
+        class spell_mogu_petrification_AuraScript : public AuraScript
+        {
+            PrepareAuraScript(spell_mogu_petrification_AuraScript);
+
+            uint32 stack;
+
+            void OnApply(constAuraEffectPtr aurEff, AuraEffectHandleModes /*mode*/)
+            {
+                if (Unit* caster = GetCaster())
+                {
+                    if (Unit* target = GetTarget())
+                    {
+                        if (target->HasAura(PETRIFIED))
+                        {
+                            stack = GetTarget()->GetAura(PETRIFIED)->GetStackAmount();
+
+                            if (stack >= 100)
+                                target->AddAura(FULLY_PETRIFIED, target);
+                        }
+                    }
+                }
+            }
+
+            void Register()
+            {
+                OnEffectApply += AuraEffectApplyFn(spell_mogu_petrification_AuraScript::OnApply, EFFECT_0, SPELL_AURA_MOD_DECREASE_SPEED, AURA_EFFECT_HANDLE_REAPPLY);
+            }
+        };
+
+        AuraScript* GetAuraScript() const
+        {
+            return new spell_mogu_petrification_AuraScript();
+        }
+};
+
+class spell_mogu_monstrous_bite : public SpellScriptLoader
+{
+    public:
+        spell_mogu_monstrous_bite() : SpellScriptLoader("spell_mogu_monstrous_bite") { }
+
+        class spell_mogu_monstrous_bite_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_mogu_monstrous_bite_SpellScript);
+
+            void HandleOnHit()
+            {
+                if (Unit* caster = GetCaster())
+                {
+                    if (Unit* target = GetHitUnit())
+                        SetHitDamage(uint32(GetHitDamage() * 2));
+                }
+            }
+            void Register()
+            {
+                OnHit += SpellHitFn(spell_mogu_monstrous_bite_SpellScript::HandleOnHit);
+            }
+        };
+
+        SpellScript* GetSpellScript() const
+        {
+            return new spell_mogu_monstrous_bite_SpellScript();
+        }
+};
+
 void AddSC_mogu_shan_vault()
 {
     new mob_cursed_mogu_sculture();
+    new mob_enormous_stone_quilen();
+    new spell_mogu_petrification();
+    new spell_mogu_monstrous_bite();
 }
