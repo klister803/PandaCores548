308c93106081fd04d61e3dee14933a6845d9f66d
Morgoporc <j.fraisse45@laposte.net>
2013-05-08 17:35:03 +0200

    Fix Soulstone can resurrect dead targets
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3150,6 +3150,9 @@ void SpellMgr::LoadSpellCustomAttr()
                 spellInfo->AttributesCu |= SPELL_ATTR0_CU_NEGATIVE_EFF0;
                 break;
             // Custom MoP Script
+            case 6203:  // Soulstone
+                spellInfo->AttributesEx2 |= SPELL_ATTR2_CAN_TARGET_DEAD;
+                break;
             case 105622:// Clemency
                 spellInfo->Effects[4].Effect = 0;
                 spellInfo->Effects[4].BasePoints = 0;
