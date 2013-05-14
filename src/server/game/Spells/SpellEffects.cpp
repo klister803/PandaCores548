cf86a274a6b00ffe83f53da9e0551c5c93198d3d
Morgoporc <j.fraisse45@laposte.net>
2013-05-14 18:35:15 +0200

    Fix Spinning Crane Kick don't heal the monk
    Signed-off-by: Morgoporc <j.fraisse45@laposte.net>

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -1595,6 +1595,10 @@ void Spell::EffectHeal(SpellEffIndex /*effIndex*/)
 
         int32 addhealth = damage;
 
+        // Spinning Crane Kick - Heal : Do not heal the monk
+        if (m_spellInfo->Id == 117640)
+            if (unitTarget->GetGUID() == m_caster->GetGUID())
+                return;
         // Vessel of the Naaru (Vial of the Sunwell trinket)
         if (m_spellInfo->Id == 45064)
         {
