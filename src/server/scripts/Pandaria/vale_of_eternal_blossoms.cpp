84068af3fd492cec5b1baf9d8812977bf862d359
Jeyel <joffrey.lacombe@voila.fr>
2013-05-23 15:25:50 +0200

    Début de script quete d'escorte
    Signed-off-by: Jeyel <joffrey.lacombe@voila.fr>

--- src/server/scripts/Pandaria/vale_of_eternal_blossoms.cpp
+++ src/server/scripts/Pandaria/vale_of_eternal_blossoms.cpp
@@ -6,6 +6,8 @@
 
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
+#include "ScriptedGossip.h"
+#include "ScriptedEscortAI.h"
 
 enum eZhaoSpells
 {
@@ -81,7 +83,83 @@ class mob_zhao_jin : public CreatureScript
         };
 };
 
+#define SAY_LAO_1   "Come quickly. There is a gap in their defenses."
+#define SAY_LAO_2   "Let's go while the way is clear. The cave is just ahead."
+#define SAY_LAO_3   "Let's wait for the next wave."
+#define SAY_LAO_4   "Stop. Let them pass."
+#define SAY_LAO_5   "Wait for a safe moment, then plant the explosives at the mouth of the cave."
+#define SAY_LAO_6   "Well, that should put a damper on their plans. Good work. I trust you can get back on your own."
+
+class npc_lao_softfoot : public CreatureScript
+{
+    public:
+        npc_lao_softfoot() : CreatureScript("npc_lao_softfoot") { }
+
+        bool OnGossipHello(Player* player, Creature* creature)
+        {
+            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Start", GOSSIP_SENDER_MAIN,  GOSSIP_ACTION_INFO_DEF);
+            
+            player->SEND_GOSSIP_MENU(player->GetGossipTextId(creature), creature->GetGUID());
+            return true;
+        }
+
+        bool OnGossipSelect(Player* player, Creature* creature, uint32 sender, uint32 action)
+        {
+            if (action == GOSSIP_ACTION_INFO_DEF)
+                creature->AI()->DoAction(1);
+            return true;
+        }
+
+        struct npc_lao_softfootAI : public npc_escortAI
+        {
+            npc_lao_softfootAI(Creature* creature) : npc_escortAI(creature) { }
+            
+            void Reset()
+            {
+
+            }
+
+            void DoAction(const int32 action)
+            {
+                if (action == 1)
+                    Start(true, true);
+            }
+
+            void WaypointReached(uint32 waypointId)
+            {
+                if (Player* player = GetPlayerForEscort())
+                {
+                    switch (waypointId)
+                    {
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            void JustDied(Unit* /*killer*/)
+            {
+                if (HasEscortState(STATE_ESCORT_ESCORTING))
+                    if (Player* player = GetPlayerForEscort())
+                        player->FailQuest(30634); // Barring Entry
+            }
+
+            void UpdateAI(const uint32 uiDiff)
+            {
+                npc_escortAI::UpdateAI(uiDiff);
+
+                DoMeleeAttackIfReady();
+            }
+        };
+
+        CreatureAI* GetAI(Creature* creature) const
+        {
+            return new npc_lao_softfootAI(creature);
+        }
+};
+
 void AddSC_vale_of_eternal_blossoms()
 {
     new mob_zhao_jin();
+    new npc_lao_softfoot();
 }
