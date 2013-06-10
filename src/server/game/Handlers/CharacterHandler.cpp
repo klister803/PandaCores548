45c669fe97b2189f87cee3ed221e587b9cdb6769
Izidor <millenium_woweur@hotmail.fr>
2013-06-10 20:07:26 +0200

    Add log
    Signed-off-by: Izidor <millenium_woweur@hotmail.fr>

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -843,7 +843,10 @@ void WorldSession::HandlePlayerLoginOpcode(WorldPacket& recvData)
     // Prevent flood of CMSG_PLAYER_LOGIN
     playerLoginCounter++;
     if (playerLoginCounter > 5)
+    {
+        sLog->OutPandashan("Player kicked due to flood of CMSG_PLAYER_LOGIN");
         KickPlayer();
+    }
     
     if (PlayerLoading() || GetPlayer() != NULL)
     {
