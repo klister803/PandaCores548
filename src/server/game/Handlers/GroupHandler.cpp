8b2ad7c67d5dc64af4a4b7a39614cbf8b6135acc
Rubaka <blackmanos@yandex.ru>
2013-08-12 23:37:58 +0300

    точных данных нема, попробуем по тыкаться

--- src/server/game/Handlers/GroupHandler.cpp
+++ src/server/game/Handlers/GroupHandler.cpp
@@ -757,25 +757,7 @@ void WorldSession::HandleRandomRollOpcode(WorldPacket& recvData)
     data << uint32(roll);
     data << uint32(minimum);
     data << uint32(maximum);
-
-    data.WriteBit(guid[0]);
-    data.WriteBit(guid[3]);
-    data.WriteBit(guid[4]);
-    data.WriteBit(guid[6]);
-    data.WriteBit(guid[1]);
-    data.WriteBit(guid[2]);
-    data.WriteBit(guid[7]);
-    data.WriteBit(guid[5]);
-
-    data.WriteByteSeq(guid[3]);
-    data.WriteByteSeq(guid[1]);
-    data.WriteByteSeq(guid[2]);
-    data.WriteByteSeq(guid[0]);
-    data.WriteByteSeq(guid[6]);
-    data.WriteByteSeq(guid[5]);
-    data.WriteByteSeq(guid[7]);
-    data.WriteByteSeq(guid[4]);
-
+    data << uint64(GetPlayer()->GetGUID());
     if (GetPlayer()->GetGroup())
         GetPlayer()->GetGroup()->BroadcastPacket(&data, false);
     else
