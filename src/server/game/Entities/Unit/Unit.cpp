736d3ddb5c065a6d5a04c69df86a62ec13d235cc
walkline <walkline.ua@gmail.com>
2013-08-22 21:53:23 +0300

    Это вернем.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -791,7 +791,7 @@ uint32 Unit::DealDamage(Unit* victim, uint32 damage, CleanDamage const* cleanDam
     // Rage from Damage made (only from direct weapon damage)
     if (cleanDamage && damagetype == DIRECT_DAMAGE && this != victim && getPowerType() == POWER_RAGE)
     {
-        uint32 rage = /*uint32(GetAttackTime(cleanDamage->attackType) / 1000 * 8.125f)*/6;
+        uint32 rage = uint32(GetAttackTime(cleanDamage->attackType) / 1000 * 8.125f);
         switch (cleanDamage->attackType)
         {
             case OFF_ATTACK:
