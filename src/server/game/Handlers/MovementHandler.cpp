48a81068345305f299800c2922814adf192d3d89
Rubaka <blackmanos@yandex.ru>
2013-08-24 19:38:55 +0300

    а ну попробуем токо пакет отослать

--- src/server/game/Handlers/MovementHandler.cpp
+++ src/server/game/Handlers/MovementHandler.cpp
@@ -257,7 +257,6 @@ void WorldSession::HandleMoveTeleportAck(WorldPacket& recvPacket)
 
     if(Unit* mover = _player->m_mover)
     {
-        mover->DestroyForNearbyPlayers();
         WorldPacket data(SMSG_MOVE_UPDATE);
         mover->m_movementInfo.time = getMSTime();
         mover->m_movementInfo.pos.m_positionX = mover->GetPositionX();
@@ -265,11 +264,12 @@ void WorldSession::HandleMoveTeleportAck(WorldPacket& recvPacket)
         mover->m_movementInfo.pos.m_positionZ = mover->GetPositionZ();
         WorldSession::WriteMovementInfo(data, &mover->m_movementInfo);
         mover->SendMessageToSet(&data, _player);
+        /*mover->DestroyForNearbyPlayers();
         sLog->outError(LOG_FILTER_NETWORKIO, "Info move %s > time: %d fall-time: %d | xyzo: %f, %f, %fo(%f) flags[%X] | Player (xyzo): %f, %f, %fo(%f) | mover (xyzo): %f, %f, %fo(%f)",
         _player->GetName(), _player->m_movementInfo.time, _player->m_movementInfo.fallTime, _player->m_movementInfo.pos.GetPositionX(), _player->m_movementInfo.pos.GetPositionY(), _player->m_movementInfo.pos.GetPositionZ(), _player->m_movementInfo.pos.GetOrientation(),
         _player->m_movementInfo.flags, _player->GetPositionX(), _player->GetPositionY(), _player->GetPositionZ(), _player->GetOrientation(),
         mover->GetPositionX(), mover->GetPositionY(), mover->GetPositionZ(), mover->GetOrientation());
-        mover->UpdateObjectVisibility();
+        mover->UpdateObjectVisibility();*/
     }
 }
 
