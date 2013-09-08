af79bb9d2921ff4df8cc6b7c9601d159b4358947
Rubaka <blackmanos@yandex.ru>
2013-09-08 18:08:19 +0300

    убираем лог причина найдена, кривая выборка режимов

--- src/server/game/Loot/LootMgr.cpp
+++ src/server/game/Loot/LootMgr.cpp
@@ -1322,9 +1322,6 @@ void LootTemplate::LootGroup::ProcessInst(Loot& loot, uint16 lootMode) const
         }
         // finish rolling
 
-        sLog->outDebug(LOG_FILTER_NETWORKIO, "WORLD: Process itemid %u, difficulty %u, spawnMode %u, GetDiffFromSpawn %u, mask %u, match %u",
-        item->itemid, item->difficulty, loot.spawnMode, sObjectMgr->GetDiffFromSpawn(loot.spawnMode), diffMask, item->difficulty & diffMask);
-
         if (item->difficulty > 0 && item->difficulty &~ diffMask)                          // Do not add if instance mode mismatch
             continue;
 
