0ad57b075c7d7b3737b11898e470a49d8e295e16
Yaki Khadafi <ElSolDolLo@gmail.com>
2013-09-18 11:56:54 +0400

    Revert "РїРѕРєР° РѕС‚РєР°С‚РёРј РїСЂР°РІРєСѓ РїР°РєРµС‚Р° РіСЂСѓРїРїС‹"
    This reverts commit bd2a8e21f83558c68a9fe023f588edbea2762845.

--- src/server/game/Groups/Group.h
+++ src/server/game/Groups/Group.h
@@ -130,11 +130,6 @@ enum GroupUpdateFlags
                         GROUP_UPDATE_FLAG_AURAS | GROUP_UPDATE_PET | GROUP_UPDATE_FLAG_PHASE // all known flags, except UNK100 and VEHICLE_SEAT
 };
 
-
-#define GROUP_UPDATE_FLAGS_COUNT          20
-                                                                // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
-static const uint8 GroupUpdateLength[GROUP_UPDATE_FLAGS_COUNT] = { 0, 2, 2, 2, 1, 2, 2, 2, 2, 4, 8, 8, 1, 2, 2, 2, 1, 2, 2, 8};
-
 class Roll : public LootValidatorRef
 {
     public:
