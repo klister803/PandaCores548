def1b1f2836d8c59829bbcf40a67f449d3f562ea
Sanmay <Sanmay@mail.ru>
2013-10-01 18:35:19 +0600

    fix boss_hoptallus
    Signed-off-by: Sanmay <Sanmay@mail.ru>

--- src/server/scripts/Pandaria/StormstoutBrewery/boss_hoptallus.cpp
+++ src/server/scripts/Pandaria/StormstoutBrewery/boss_hoptallus.cpp
@@ -12,6 +12,7 @@ enum Spells
 {
     SPELL_CARROT_BREATH  = 112944,
     SPELL_FURL_WIND      = 112992, 
+    SPELL_EXPLOSIVE_BREW = 114291,
 
 };
 
@@ -30,19 +31,27 @@ class boss_hoptallus : public CreatureScript
             InstanceScript* instance;
             uint32 breathtimer;
             uint32 windtimer;
+            uint32 summonadd;
          
             void Reset()
             {
                 _Reset();
                 breathtimer = 0;
                 windtimer = 0;
+                summonadd = 0;
             }
 
             void EnterCombat(Unit* /*who*/)
             {
                 _EnterCombat();
-                breathtimer = 5000;
-                windtimer = 12000;
+                breathtimer = 13000;
+                windtimer = 8000;
+                summonadd = 10000;
+            }
+
+            void JustSummoned(Creature* summon)
+            {
+                summons.Summon(summon);
             }
 
             void JustDied(Unit* /*killer*/)
@@ -61,7 +70,7 @@ class boss_hoptallus : public CreatureScript
                 if (breathtimer <= diff)
                 {
                     DoCast(me, SPELL_CARROT_BREATH);
-                    breathtimer = 8000;
+                    breathtimer = 13000;
                 }
                 else
                     breathtimer -= diff;
@@ -69,10 +78,20 @@ class boss_hoptallus : public CreatureScript
                 if (windtimer <= diff)
                 {
                     DoCast(me, SPELL_FURL_WIND);
-                    windtimer = 12000;
+                    windtimer = 8000;
                 }
                 else
                     windtimer -= diff;
+
+                if (summonadd <= diff)
+                {
+                    for (uint8 i = 0; i < 3; i++) 
+                       if (Creature* hopper =  me->SummonCreature(59464, me->GetPositionX() +5, me->GetPositionY(), me->GetPositionZ(), TEMPSUMMON_CORPSE_DESPAWN))
+                           hopper->SetInCombatWithZone();
+                       summonadd = 10000;
+                }
+                else
+                    summonadd -= diff;
 
                 DoMeleeAttackIfReady();
             }
@@ -84,8 +103,50 @@ class boss_hoptallus : public CreatureScript
         }
 };
 
+class npc_hopper : public CreatureScript
+{
+    public:
+        npc_hopper() : CreatureScript("npc_hopper") {}
+
+        struct npc_hopperAI : public ScriptedAI
+        {
+            npc_hopperAI(Creature* creature) : ScriptedAI(creature){}
+
+            bool explose;
+
+            void Reset()
+            {
+                explose = false;
+            }
+
+            void UpdateAI(const uint32 diff)
+            {
+                if (me->HasUnitState(UNIT_STATE_CASTING))
+                    return;
+
+                if (me->getVictim())
+                {
+                    if (me->IsWithinMeleeRange(me->getVictim()))
+                    {
+                        if (me->GetDistance(me->getVictim()) <= 0.5f && !explose)
+                        {
+                            explose = true;
+                            DoCast(me->getVictim(), SPELL_EXPLOSIVE_BREW);
+                        }
+                    }
+                }
+            }
+        };
+
+        CreatureAI* GetAI(Creature* creature) const
+        {
+            return new npc_hopperAI(creature);
+        }
+};
+
 
 void AddSC_boss_hoptallus()
 {
     new boss_hoptallus();
+    new npc_hopper();
 }
