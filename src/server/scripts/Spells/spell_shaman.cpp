c18fbeb41a696e3c2957567c4dd4ba343f4db8cd
Yaki Khadafi <ElSolDolLo@gmail.com>
2013-10-09 20:59:48 +0400

    adjust distance

--- src/server/scripts/Spells/spell_shaman.cpp
+++ src/server/scripts/Spells/spell_shaman.cpp
@@ -1747,28 +1747,28 @@ class spell_shaman_totemic_projection : public SpellScriptLoader
                 if(Creature* totem = caster->GetMap()->GetCreature(caster->m_SummonSlot[1]))
                 {
                     Position pos;
-                    summon->GetFirstCollisionPosition(pos, 5.0f, static_cast<float>(-M_PI/4));
+                    summon->GetFirstCollisionPosition(pos, 2.5f, static_cast<float>(-M_PI/4));
                     totem->NearTeleportTo(pos.GetPositionX(), pos.GetPositionY(), pos.GetPositionZ(), totem->GetOrientation());
                     //totem->SendMovementFlagUpdate();
                 }
                 if(Creature* totem = caster->GetMap()->GetCreature(caster->m_SummonSlot[2]))
                 {
                     Position pos;
-                    summon->GetFirstCollisionPosition(pos, 5.0f, static_cast<float>(-3*M_PI/4));
+                    summon->GetFirstCollisionPosition(pos, 2.5f, static_cast<float>(-3*M_PI/4));
                     totem->NearTeleportTo(pos.GetPositionX(), pos.GetPositionY(), pos.GetPositionZ(), totem->GetOrientation());
                     //totem->SendMovementFlagUpdate();
                 }
                 if(Creature* totem = caster->GetMap()->GetCreature(caster->m_SummonSlot[3]))
                 {
                     Position pos;
-                    summon->GetFirstCollisionPosition(pos, 5.0f, static_cast<float>(3*M_PI/4));
+                    summon->GetFirstCollisionPosition(pos, 2.5f, static_cast<float>(3*M_PI/4));
                     totem->NearTeleportTo(pos.GetPositionX(), pos.GetPositionY(), pos.GetPositionZ(), totem->GetOrientation());
                     //totem->SendMovementFlagUpdate();
                 }
                 if(Creature* totem = caster->GetMap()->GetCreature(caster->m_SummonSlot[4]))
                 {
                     Position pos;
-                    summon->GetFirstCollisionPosition(pos, 5.0f, static_cast<float>(M_PI/4));
+                    summon->GetFirstCollisionPosition(pos, 2.5f, static_cast<float>(M_PI/4));
                     totem->NearTeleportTo(pos.GetPositionX(), pos.GetPositionY(), pos.GetPositionZ(), totem->GetOrientation());
                     //totem->SendMovementFlagUpdate();
                 }
