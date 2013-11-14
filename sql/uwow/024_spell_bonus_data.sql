fd97df117d43d9e83fb40ad31a365a8550408324
ysmart <zen.89-89@mail.ru>
2013-11-14 22:46:44 +0200

    typo k 6c94838

--- sql/uwow/024_spell_bonus_data.sql
+++ sql/uwow/024_spell_bonus_data.sql
@@ -2,4 +2,4 @@ DELETE FROM `spell_bonus_data` WHERE (`entry`='974');
 INSERT INTO `spell_bonus_data` (`entry`, `comments`) VALUES ('974', 'Shaman - Earth Shield');
 
 DELETE FROM `spell_bonus_data` WHERE (`entry`='379');
-INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `comments`) VALUES ('379', '0.1625', 'Shaman - Earth Shield Triggered');
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `comments`) VALUES ('379', '0.13', 'Shaman - Earth Shield Triggered');
