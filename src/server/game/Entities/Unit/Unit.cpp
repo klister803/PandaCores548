29a6e46862b64eec3c089772265e9ef83193f0ec
ysmart <zen.89-89@mail.ru>
2013-11-18 22:51:35 +0200

    60) / Заклинание / Друид: Переписан расчет накопления ярости в форме "Облик медведя".

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -789,10 +789,14 @@ uint32 Unit::DealDamage(Unit* victim, uint32 damage, CleanDamage const* cleanDam
             case OFF_ATTACK:
                 rage /= 2;
             case BASE_ATTACK:
+            {
                 if (ToPlayer() && ToPlayer()->getClass() == CLASS_WARRIOR)
                     rage = 6;
+                if (ToPlayer() && ToPlayer()->getClass() == CLASS_DRUID)
+                    rage = 4;
                 RewardRage(rage, true);
                 break;
+            }
             default:
                 break;
         }
