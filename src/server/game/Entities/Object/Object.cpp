c90583b481ec0f59f41d35370ce202c32cbf2b56
Yaki Khadafi <ElSolDolLo@gmail.com>
2013-11-21 00:54:59 +0300

    SMSG_DESTROY_OBJECT

--- src/server/game/Entities/Object/Object.cpp
+++ src/server/game/Entities/Object/Object.cpp
@@ -304,12 +304,15 @@ void Object::BuildOutOfRangeUpdateBlock(UpdateData* data) const
 void Object::DestroyForPlayer(Player* target, bool onDeath) const
 {
     ASSERT(target);
+    ObjectGuid guid = GetObjectGuid();
 
     WorldPacket data(SMSG_DESTROY_OBJECT, 8 + 1);
-    data << uint64(GetGUID());
+    data.WriteGuidMask<3, 5, 4, 6, 7, 2, 0>(guid);
     //! If the following bool is true, the client will call "void CGUnit_C::OnDeath()" for this object.
     //! OnDeath() does for eg trigger death animation and interrupts certain spells/missiles/auras/sounds...
-    data << uint8(onDeath ? 1 : 0);
+    data.WriteBit(onDeath);
+    data.WriteGuidMask<1>(guid);
+    data.WriteGuidBytes<4, 2, 0, 3, 7, 1, 5, 6>(guid);
     target->GetSession()->SendPacket(&data);
 }
 
