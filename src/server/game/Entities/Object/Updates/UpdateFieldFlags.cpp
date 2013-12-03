e3f64e258d70720162e2bab3f2f98f11e473d249
Yaki Khadafi <ElSolDolLo@gmail.com>
2013-12-03 13:17:27 +0300

    -_\\\

--- src/server/game/Entities/Object/Updates/UpdateFieldFlags.cpp
+++ src/server/game/Entities/Object/Updates/UpdateFieldFlags.cpp
@@ -2261,4 +2261,4 @@ uint32 UnitDynamicFieldFlags[PLAYER_DYNAMIC_END] =
 uint32 ItemDynamicFieldFlags[ITEM_DYNAMIC_END] =
 {
     UF_FLAG_OWNER,                                          // ITEM_DYNAMIC_MODIFIERS
-},
+};
