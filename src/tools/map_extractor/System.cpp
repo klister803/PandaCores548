e40f05a3b9bec44244c1de963689c7287aa4081b
Yaki Khadafi <ElSolDolLo@gmail.com>
2014-03-03 01:38:51 +0300

    fix map z extraction

--- src/tools/map_extractor/System.cpp
+++ src/tools/map_extractor/System.cpp
@@ -576,7 +576,7 @@ bool ConvertADT(char *filename, char *filename2, int /*cell_y*/, int /*cell_x*/,
             }
             // Get custom height
             adt_MCVT *v = cell->getMCVT();
-            //if (!v)
+            if (!v)
                 continue;
             // get V9 height map
             for (int y=0; y <= ADT_CELL_SIZE; y++)
