72e3ec938560fd71b2c9b69fbd881e32b1e2ce23
antonskutin <konfitu@mail.ru>
2014-04-21 01:04:02 +0600

    Еще правка к броску

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -4672,7 +4672,9 @@ void SpellMgr::LoadSpellCustomAttr()
                 case 1543: // Flare. Hack from wait to fix areatrigger
                     spellInfo->Effects[0].TriggerSpell = 94528;
                     break;
-                
+                case 64382: // Shattering Throw
+                    spellInfo->AttributesEx |= SPELL_ATTR1_DISPEL_AURAS_ON_IMMUNITY;
+                    break;
                 default:
                     break;
             }
