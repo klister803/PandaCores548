b35807831b94a1693c77e72765df11561bbe4475
cyberbrest <cyberbrest@cyberbrest.com>
2014-05-02 17:09:56 +0300

    [DB]: thx Kadafy for pointing issue.

--- sql/uwow/2014_05_02_00_world_creature.sql
+++ sql/uwow/2014_05_02_00_world_creature.sql
@@ -1,1 +1,2 @@
-DELETE FROM `creature_template_addon` WHERE `auras` LIKE '%29266%';
+UPDATE `creature_template_addon` SET auras = NULL WHERE `auras` LIKE '29266';
+UPDATE `creature_template_addon` SET auras = REPLACE(auras, '29266', ' ');
