80eb122d11ebe60e246470f07cf41775cbbc7e65
cyberbrest <cyberbrest@cyberbrest.com>
2014-05-04 12:38:04 +0300

    [Pet/test]:  fix overflow pet guid by  pet number + fix summoning, spellbar and etc.  ToDo: check is it good?

--- src/server/game/Entities/Pet/Pet.cpp
+++ src/server/game/Entities/Pet/Pet.cpp
@@ -1900,7 +1900,7 @@ bool Pet::Create(uint32 guidlow, Map* map, uint32 phaseMask, uint32 Entry, uint3
     SetMap(map);
 
     SetPhaseMask(phaseMask, false);
-    Object::_Create(guidlow, pet_number, HIGHGUID_PET);
+    Object::_Create(pet_number, guidlow, HIGHGUID_PET);
 
     m_DBTableGuid = guidlow;
     m_originalEntry = Entry;
