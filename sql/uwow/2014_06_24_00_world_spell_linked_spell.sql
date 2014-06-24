ac12f72e43cb4cf7b9bd68784c544d81e34096c0
antonskutin <konfitu@mail.ru>
2014-06-24 06:19:55 +0600

    Item - Warrior PvP Set 4P Bonus

---
+++ sql/uwow/2014_06_24_00_world_spell_linked_spell.sql
@@ -0,0 +1,1 @@
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `caster`, `target`, `hastalent`, `hastalent2`, `chance`, `cooldown`, `type2`, `hitmask`, `learnspell`, `comment`) VALUES (6544, 133278, 0, 0, 0, 133277, 0, 0, 0, 0, 0, 0, 'Item - Warrior PvP Set 4P Bonus');
