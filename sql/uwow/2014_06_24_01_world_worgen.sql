d9c0b593733f08669a94902ceefdaab77f709977
cyberbrest <cyberbrest@cyberbrest.com>
2014-07-01 20:06:21 +0300

    [Quest/Worgen]: Phase. Fix final phase.

--- sql/uwow/2014_06_24_01_world_worgen.sql
+++ sql/uwow/2014_06_24_01_world_worgen.sql
@@ -25,12 +25,12 @@ DELETE FROM `spell_area` WHERE `spell` in (67789) AND area in (4755);
 INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES 
 ('67789', '4755', '14222', '0', '0', '0', '2', '1', '10', '11');
 
--- After complete quest 24902
+-- After start quest 24904
 DELETE FROM  `quest_end_scripts` WHERE  `id` =24902;
 UPDATE `quest_template` SET `CompleteScript` = '0' WHERE `Id` = 24902;
 DELETE FROM `spell_area` WHERE `spell` in (69486) AND area in (4755);
 INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES 
-('69486', '4755', '24902', '0', '0', '0', '2', '1', '74', '11');
+('69486', '4755', '24904', '0', '0', '0', '2', '1', '74', '0');
 
 -- --------------------------
 -- Worgen Second Zone - 4714
