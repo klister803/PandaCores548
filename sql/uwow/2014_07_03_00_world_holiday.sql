f010d867c6675889a63c0a3a55a985f4d267c028
cyberbrest <cyberbrest@cyberbrest.com>
2014-07-03 15:15:09 +0300

    [Holiday]: Achieve 8043

---
+++ sql/uwow/2014_07_03_00_world_holiday.sql
@@ -0,0 +1,3 @@
+INSERT INTO `gameobject` (`id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `isActive`) VALUES
+( 217851, 870, 5840, 5840, 1, 1, 1322.29, 460.914, 457.784, 3.87661, 0, 0, 0.933225, -0.359292, 300, 0, 1, 0);
+INSERT INTO game_event_gameobject (eventEntry, guid) SELECT 1, guid FROM gameobject WHERE id = 187892;
