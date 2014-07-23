d26f649f3e6ed070e772ad8fe7b980795b9b16c6
cyberbrest <cyberbrest@cyberbrest.com>
2014-07-23 12:43:01 +0300

    Revert "Общее: Добавляем CheckCast для попытки бафать петов."
    This reverts commit c811439b84cbcf1990e844bd233452ebc754edd3.

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -3031,8 +3031,7 @@ void UnitAura::FillTargetMap(std::map<Unit*, uint32> & targets, Unit* caster)
             {
                 if (m_spellInfo->AttributesEx7 & SPELL_ATTR7_CONSOLIDATED_RAID_BUFF)
                     if ((*itr)->GetTypeId() != TYPEID_PLAYER && (*itr) != caster)
-                        if ((*itr)->GetOwner() && (*itr)->GetOwner()->GetTypeId() == TYPEID_PLAYER)
-                            continue;
+                        continue;
 
                 targets[*itr] = 1<<effIndex;
             }
