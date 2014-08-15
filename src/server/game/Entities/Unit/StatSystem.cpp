c993512da6662beb4173889edd50a2cea54bee96
antonskutin <konfitu@mail.ru>
2014-08-15 23:16:47 +0600

    Typo

--- src/server/game/Entities/Unit/StatSystem.cpp
+++ src/server/game/Entities/Unit/StatSystem.cpp
@@ -731,8 +731,6 @@ void Player::UpdateRangedHitChances()
 {
     m_modRangedHitChance = (float)GetTotalAuraModifier(SPELL_AURA_MOD_HIT_CHANCE);
     m_modRangedHitChance += GetRatingBonusValue(CR_HIT_RANGED);
-    
-    SetFloatValue(PLAYER_FIELD_UI_SPELL_HIT_MODIFIER, m_modRangedHitChance);
 }
 
 void Player::UpdateSpellHitChances()
