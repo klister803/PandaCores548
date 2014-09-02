78f7bbaf4059f16b513ce54ca9a192b059c2ea0b
cyberbrest <cyberbrest@cyberbrest.com>
2014-09-02 22:03:49 +0300

    [Crash]...

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -2369,7 +2369,7 @@ bool Aura::CanStackWith(Aura const* existingAura) const
         }
     }
 
-    if (HasEffectType(SPELL_AURA_CONTROL_VEHICLE) && existingAura->HasEffectType(SPELL_AURA_CONTROL_VEHICLE))
+    if (GetSpellInfo()->HasAura(SPELL_AURA_CONTROL_VEHICLE) && existingAura->GetSpellInfo()->HasAura(SPELL_AURA_CONTROL_VEHICLE))
     {
         Vehicle* veh = NULL;
         if (GetOwner()->ToUnit())
