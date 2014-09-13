f5da9a0672485b1552d7cbc90783bd8ba15f8e07
cyberbrest <cyberbrest@cyberbrest.com>
2014-09-13 12:37:22 +0300

    return....

--- src/server/game/Entities/Object/Object.cpp
+++ src/server/game/Entities/Object/Object.cpp
@@ -2708,6 +2708,13 @@ TempSummon* WorldObject::SummonCreature(uint32 entry, const Position &pos, uint6
 {
     if (Map* map = FindMap())
     {
+        if(!ToUnit())
+        {
+            std::list<Creature*> creatures;
+            GetAliveCreatureListWithEntryInGrid(creatures, entry, 110.0f);
+            if(creatures.size() > 50)
+                return NULL;
+        }
         if (TempSummon* summon = map->SummonCreature(entry, pos, properties, duration, isType(TYPEMASK_UNIT) ? (Unit*)this : NULL, targetGuid, spellId))
         {
             summon->SetTempSummonType(spwtype);
@@ -2722,6 +2729,13 @@ TempSummon* WorldObject::SummonCreature(uint32 entry, const Position &pos, TempS
 {
     if (Map* map = FindMap())
     {
+        if(!ToUnit())
+        {
+            std::list<Creature*> creatures;
+            GetAliveCreatureListWithEntryInGrid(creatures, entry, 110.0f);
+            if(creatures.size() > 50)
+                return NULL;
+        }
         if (TempSummon* summon = map->SummonCreature(entry, pos, NULL, duration, isType(TYPEMASK_UNIT) ? (Unit*)this : NULL, 0, 0, vehId, viewerGuid, viewersList))
         {
             summon->SetTempSummonType(spwtype);
