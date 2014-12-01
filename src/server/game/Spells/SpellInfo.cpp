a612c7c82c7280c5c5395390e7742dae1a450a9e
antonskutin <konfitu@mail.ru>
2014-12-01 10:44:12 +0500

    Вернем эту часть в дискавери.

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -1246,10 +1246,9 @@ bool SpellInfo::HasDynAuraEffect() const
 bool SpellInfo::IsExplicitDiscovery() const
 {
     return ((Effects[0].Effect == SPELL_EFFECT_CREATE_RANDOM_ITEM
-        || Effects[0].Effect == SPELL_EFFECT_CREATE_ITEM
         || Effects[0].Effect == SPELL_EFFECT_CREATE_ITEM_2)
-        && (Effects[1].Effect == SPELL_EFFECT_SCRIPT_EFFECT
-        || Effects[0].Effect == SPELL_EFFECT_SCRIPT_EFFECT));
+        && Effects[1].Effect == SPELL_EFFECT_SCRIPT_EFFECT)
+        || Id == 64323 || Id == 101805;
 }
 
 bool SpellInfo::IsLootCrafting() const
