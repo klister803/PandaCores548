4ec53317b3ff88c71fada129d982d4a5d9b22fce
konfitu <konfitu@mail.ru>
2015-05-20 16:54:39 +0500

    Добавляем поддержку для кулинарных сумок

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -215,6 +215,10 @@ bool ItemCanGoIntoBag(ItemTemplate const* pProto, ItemTemplate const* pBagProto)
                     if (!(pProto->BagFamily & BAG_FAMILY_MASK_FISHING_SUPP))
                         return false;
                     return true;
+                case ITEM_SUBCLASS_COOKING_BAG:
+                    if (!(pProto->BagFamily & BAG_FAMILY_MASK_COOKING_SUPP))
+                        return false;
+                    return true;
                 default:
                     return false;
             }
