f3c5fdc307cd94baaa389d10ffa47cea35af1d1e
Shadowstrike <Shadowstrike@gmail.com>
2015-06-03 16:15:53 +0600

    [Warden]: move action table to world database
    Signed-off-by: Shadowstrike <Shadowstrike@gmail.com>

--- src/server/game/Warden/WardenCheckMgr.cpp
+++ src/server/game/Warden/WardenCheckMgr.cpp
@@ -160,11 +160,11 @@ void WardenCheckMgr::LoadWardenOverrides()
     }
 
     //                                                      0        1
-    QueryResult result = CharacterDatabase.Query("SELECT wardenId, action FROM warden_action");
+    QueryResult result = WorldDatabase.Query("SELECT wardenId, action FROM warden_action");
 
     if (!result)
     {
-        sLog->outInfo(LOG_FILTER_SERVER_LOADING, ">> Loaded 0 Warden action overrides. DB table `warden_action` is empty!");
+        sLog->outInfo(LOG_FILTER_SERVER_LOADING, ">> Loaded 0 Warden actions. DB table `warden_action` is empty!");
         return;
     }
 
@@ -193,7 +193,7 @@ void WardenCheckMgr::LoadWardenOverrides()
     }
     while (result->NextRow());
 
-    sLog->outInfo(LOG_FILTER_WARDEN, ">> Loaded %u warden action overrides.", count);
+    sLog->outInfo(LOG_FILTER_WARDEN, ">> Loaded %u warden actions", count);
 
 }
 
