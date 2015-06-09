ddd5a3895382b6a4f9439922d85cff9534b7229a
Yourp <zen.89-89@mail.ru>
2015-06-10 00:14:08 +0300

    Шаман: Переписан талант Освобожденная ярость

--- src/server/scripts/Spells/spell_shaman.cpp
+++ src/server/scripts/Spells/spell_shaman.cpp
@@ -593,7 +593,7 @@ class spell_sha_unleash_elements : public SpellScriptLoader
                             }
 
                             if (hostileSpell && !hostileTarget)
-                                return; // don't allow to attack non-hostile targets. TODO: check this before cast
+                                continue; // don't allow to attack non-hostile targets. TODO: check this before cast
 
                             if (!hostileSpell && hostileTarget)
                                 target = _player;   // heal ourselves instead of the enemy
@@ -602,45 +602,6 @@ class spell_sha_unleash_elements : public SpellScriptLoader
                                 _player->CastSpell(target, unleashSpell, true);
 
                             target = GetExplTargetUnit();
-
-                            // If weapons are enchanted by same enchantment, only one should be unleashed
-                            if (i == 0 && weapons[0] && weapons[1])
-                                if (weapons[0]->GetEnchantmentId(TEMP_ENCHANTMENT_SLOT) == weapons[1]->GetEnchantmentId(TEMP_ENCHANTMENT_SLOT))
-                                    return;
-                        }
-                    }
-                    if (_player->HasAura(SPELL_SHA_UNLEASHED_FURY_TALENT))
-                    {
-                        Item *weapons[2];
-                        weapons[0] = _player->GetItemByPos(INVENTORY_SLOT_BAG_0, EQUIPMENT_SLOT_MAINHAND);
-                        weapons[1] = _player->GetItemByPos(INVENTORY_SLOT_BAG_0, EQUIPMENT_SLOT_OFFHAND);
-
-                        for (int i = 0; i < 2; i++)
-                        {
-                            if(!weapons[i])
-                                continue;
-
-                            uint32 furySpell = 0;
-                            switch (weapons[i]->GetEnchantmentId(TEMP_ENCHANTMENT_SLOT))
-                            {
-                                case 3345: // Earthliving Weapon
-                                    furySpell = SPELL_SHA_UNLEASHED_FURY_EARTHLIVING;
-                                    break;
-                                case 5: // Flametongue Weapon
-                                    furySpell = SPELL_SHA_UNLEASHED_FURY_FLAMETONGUE;
-                                    break;
-                                case 2: // Frostbrand Weapon
-                                    furySpell = SPELL_SHA_UNLEASHED_FURY_FROSTBRAND;
-                                    break;
-                                case 3021: // Rockbiter Weapon
-                                    furySpell = SPELL_SHA_UNLEASHED_FURY_ROCKBITER;
-                                    break;
-                                case 283: // Windfury Weapon
-                                    furySpell = SPELL_SHA_UNLEASHED_FURY_WINDFURY;
-                                    break;
-                            }
-                            if (furySpell)
-                                _player->CastSpell(_player, furySpell, true);
 
                             // If weapons are enchanted by same enchantment, only one should be unleashed
                             if (i == 0 && weapons[0] && weapons[1])
