c30ae4be712f47bdf43ed372bebf0fe7422a618a
Rubaka <blackmanos@yandex.ru>
2015-07-09 07:08:51 +0300

    удаляем

--- sql/uwow/2015_07_07_00_world_misc.sql
+++ sql/uwow/2015_07_07_00_world_misc.sql
@@ -11,3 +11,4 @@ insert into `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `cast
 insert into `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `SpellFamilyMask3`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`, `effectmask`) values
 ('145152','0','0','0','0','0','0','4112','0','0','0','0','7');
 
+DELETE FROM spell_linked_spell WHERE spell_trigger = -22812 AND spell_effect = 132402;
