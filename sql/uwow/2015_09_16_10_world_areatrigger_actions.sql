c94f20e42ddfd7b77336fd067bb1946f9bf21062
Yourp <zen.89-89@mail.ru>
2015-09-16 17:27:28 +0300

    Охотник: Исправлен баг, когда Ледяная ловушка не замедляла противников, которые находились под эффектом незаметности.

--- sql/uwow/2015_09_16_10_world_areatrigger_actions.sql
+++ sql/uwow/2015_09_16_10_world_areatrigger_actions.sql
@@ -1,2 +1,5 @@
 UPDATE `areatrigger_actions` SET `moment`='1', `targetFlags`='4096' WHERE (`entry`='510') AND (`id`='0');
 UPDATE `areatrigger_actions` SET `targetFlags`='4096' WHERE (`entry`='510') AND (`id`='1');
+
+UPDATE `areatrigger_actions` SET `targetFlags`='4096' WHERE (`entry`='621') AND (`id`='0');
+UPDATE `areatrigger_actions` SET `moment`='42', `targetFlags`='4096' WHERE (`entry`='621') AND (`id`='1');
