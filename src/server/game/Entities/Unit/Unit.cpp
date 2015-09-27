4c8eccbbeadaca593f55783a3086bb04aade2c55
Yourp <zen.89-89@mail.ru>
2015-09-27 17:26:49 +0300

    Делаем, что бы эффект SPELL_AURA_SPELL_MAGNET снимался после попадания спела, а не после вылета.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -11873,7 +11873,7 @@ Unit* Unit::GetMagicHitRedirectTarget(Unit* victim, SpellInfo const* spellInfo)
                 && _IsValidAttackTarget(magnet, spellInfo))
             {
                 // TODO: handle this charge drop by proc in cast phase on explicit target
-                (*itr)->GetBase()->DropCharge(AURA_REMOVE_BY_DROP_CHARGERS);
+                //(*itr)->GetBase()->DropCharge(AURA_REMOVE_BY_DROP_CHARGERS);
                 return magnet;
             }
     }
