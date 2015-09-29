a896522357ad08cfc91aa679151f2ef577e7d737
Yourp <zen.89-89@mail.ru>
2015-09-30 02:58:22 +0300

    Шаман: Исправлен баг, когда Щит земли срабатывал от ненаносящих урон эффектов.

--- sql/uwow/2015_09_27_10_world_misc.sql
+++ sql/uwow/2015_09_27_10_world_misc.sql
@@ -1,3 +1,6 @@
 UPDATE `creature_template` SET `spell2`='0' WHERE (`entry`='5925');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('8178', 'spell_sha_grounding_totem');
 UPDATE `spell_proc_event` SET `procFlags`='139264' WHERE (`entry`='8178') AND (`effectmask`='7');
+
+DELETE FROM `spell_proc_event` WHERE (`entry`='974') AND (`effectmask`='7');
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`) VALUES ('974', '1048576');
