b2d67aa9bf5390e1eb713c12681d9c2034d1d209
Yourp <zen.89-89@mail.ru>
2015-10-01 12:49:11 +0300

    Охотник: Теперь талант Охотничий азарт будет срабатывать только от атак дальнего боя требующих затрат концентраций.

--- sql/uwow/2015_09_27_10_world_misc.sql
+++ sql/uwow/2015_09_27_10_world_misc.sql
@@ -4,3 +4,12 @@ UPDATE `spell_proc_event` SET `procFlags`='139264' WHERE (`entry`='8178') AND (`
 
 DELETE FROM `spell_proc_event` WHERE (`entry`='974') AND (`effectmask`='7');
 INSERT INTO `spell_proc_event` (`entry`, `procFlags`) VALUES ('974', '1048576');
+
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `hastalent`, `chance`, `hitmask`, `comment`) VALUES ('34026', '34720', '109306', '30', '1', 'Thrill of the Hunt');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `hastalent`, `chance`, `hitmask`, `comment`) VALUES ('117050', '34720', '109306', '30', '1', 'Thrill of the Hunt');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `hastalent`, `chance`, `hitmask`, `comment`) VALUES ('131894', '34720', '109306', '30', '1', 'Thrill of the Hunt');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `hastalent`, `chance`, `hitmask`, `comment`) VALUES ('109259', '34720', '109306', '30', '1', 'Thrill of the Hunt');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `hastalent`, `chance`, `hitmask`, `comment`) VALUES ('3674', '34720', '109306', '30', '1', 'Thrill of the Hunt');
+
+DELETE FROM `spell_proc_event` WHERE `entry` IN (109306);
+INSERT INTO `spell_proc_event` VALUES (109306, 0, 0, 2048+4096+131072, 2147483648, 16777216+256+1, 4+8192, 0, 0, 0, 0, 0, 7);
