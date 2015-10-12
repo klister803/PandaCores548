3e22c93217ccda9a187d79b27ed84e03c959ae5b
Yourp <zen.89-89@mail.ru>
2015-10-12 19:10:53 +0300

    Жрец: Теперь скорость жреца не будет влиять на скорость произнесения заклинаний Ментального демона.

--- sql/uwow/2015_10_07_10_world_misc.sql
+++ sql/uwow/2015_10_07_10_world_misc.sql
@@ -4,3 +4,5 @@ DELETE FROM `spell_script_names` WHERE (`spell_id`='108212') AND (`ScriptName`='
 
 DELETE FROM `spell_linked_spell` WHERE (`spell_trigger`='33395') AND (`spell_effect`='44544') AND (`type`='0') AND (`hastalent`='0');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('33395', 'spell_mage_pet_freeze');
+
+UPDATE `pet_stats` SET `haste`='0' WHERE (`entry`='59190');
