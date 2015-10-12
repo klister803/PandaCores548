6dac86527884f0def92b3a699abd4398d7e1793a
Yourp <zen.89-89@mail.ru>
2015-10-12 21:08:55 +0300

    Чернокнижник: Исправлен баг с уменьшением скорости Охотника Скверны.

--- sql/uwow/2015_10_07_10_world_misc.sql
+++ sql/uwow/2015_10_07_10_world_misc.sql
@@ -6,3 +6,5 @@ DELETE FROM `spell_linked_spell` WHERE (`spell_trigger`='33395') AND (`spell_eff
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('33395', 'spell_mage_pet_freeze');
 
 UPDATE `pet_stats` SET `haste`='0' WHERE (`entry`='59190');
+
+UPDATE `creature_template` SET `speed_run`='1.14286' WHERE (`entry`='417');
