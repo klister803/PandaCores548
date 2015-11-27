506c21e6f1200185c65677b68e1b95121e05f972
Nikita Starikov <patheticneatness@gmail.com>
2015-11-28 03:05:57 +0700

    Туда-же

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -350,7 +350,7 @@ Unit::~Unit()
     ASSERT(!m_attacking);
     ASSERT(m_attackers.empty());
     ASSERT(m_sharedVision.empty());
-    ASSERT(m_Controlled.empty());
+    //ASSERT(m_Controlled.empty()); // @todo reimplement this
     ASSERT(m_appliedAuras.empty());
     ASSERT(m_ownedAuras.empty());
     ASSERT(m_removedAuras.empty());
