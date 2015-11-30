f29bfa3e57a320071ab00958d842859ec153292e
Rubaka <blackmanos@yandex.ru>
2015-11-30 21:33:01 +0300

    Revert "Возвращаем небольшую оптимизацию достижений"
    This reverts commit 568b50f915e6399b482e59e2e3fd1102875020f2.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -4182,7 +4182,6 @@ void Unit::RemoveAura(AuraApplication * aurApp, AuraRemoveMode mode)
     // no need to remove
     if (aurApp->GetBase()->GetApplicationOfTarget(GetGUID()) != aurApp || aurApp->GetBase()->IsRemoved())
         return;
-
     uint32 spellId = aurApp->GetBase()->GetId();
 
     if (spellId == 51713 && mode != AURA_REMOVE_BY_EXPIRE)
@@ -4541,7 +4540,6 @@ void Unit::RemoveAreaAurasDueToLeaveWorld()
             Unit* target = aurApp->GetTarget();
             if (!target || target == this)
                 continue;
-
             target->RemoveAura(aurApp);
             // things linked on aura remove may apply new area aura - so start from the beginning
             iter = m_ownedAuras.begin();
@@ -12123,7 +12121,7 @@ void Unit::RemoveAllControlled()
             else if (target->GetOwnerGUID() == GetGUID() && target->isSummon())
                 target->ToTempSummon()->UnSummon();
         }
-        else if (target->ToTempSummon() && target->IsInWorld())
+        else if (target->ToTempSummon())
             target->ToTempSummon()->UnSummon();
         else
             sLog->outError(LOG_FILTER_UNITS, "Unit %u is trying to release unit %u which is neither charmed nor owned by it", GetEntry(), target->GetEntry());
@@ -17180,7 +17178,7 @@ void Unit::RemoveFromWorld()
         if (!IsVehicle()) // should be remove in deathstate
             RemoveAllControlled();
 
-        if (!ToCreature())
+        if(!ToCreature())
             RemoveAreaAurasDueToLeaveWorld();
 
         if (GetCharmerGUID())
--- src/server/game/Maps/MapUpdater.cpp
+++ src/server/game/Maps/MapUpdater.cpp
@@ -1,112 +1,131 @@
-#include <mutex>
-#include <condition_variable>
-
 #include "MapUpdater.h"
+#include "DelayExecutor.h"
 #include "Map.h"
+#include "DatabaseEnv.h"
 
-/// Constructor
-MapUpdaterTask::MapUpdaterTask(MapUpdater *updater) : _updater(updater)
+#include <ace/Guard_T.h>
+#include <ace/Method_Request.h>
+
+class WDBThreadStartReq1 : public ACE_Method_Request
 {
-
-}
-
-/// Notify that the task is done
-void MapUpdaterTask::UpdateFinished()
-{
-    if (_updater != nullptr)
-        _updater->update_finished();
-}
-
-
-class MapUpdateRequest : MapUpdaterTask
-{
-    private:
-        Map &_map;
-        uint32 _diff;
-
     public:
-        MapUpdateRequest(Map &map, MapUpdater &updater, uint32 diff) :
-            MapUpdaterTask(&updater), _map(map), _diff(diff)
+
+        WDBThreadStartReq1()
         {
         }
 
-        void call() override
+        virtual int call()
         {
-            _map.Update(_diff);
-            UpdateFinished();
+            return 0;
         }
 };
 
-void MapUpdater::activate(size_t num_threads)
+class WDBThreadEndReq1 : public ACE_Method_Request
 {
-    for (size_t i = 0; i < num_threads; ++i)
-    {
-        _workerThreads.push_back(std::thread(&MapUpdater::WorkerThread, this));
-    }
+    public:
+
+        WDBThreadEndReq1()
+        {
+        }
+
+        virtual int call()
+        {
+            return 0;
+        }
+};
+
+class MapUpdateRequest : public ACE_Method_Request
+{
+    private:
+
+        Map& m_map;
+        MapUpdater& m_updater;
+        ACE_UINT32 m_diff;
+
+    public:
+
+        MapUpdateRequest(Map& m, MapUpdater& u, ACE_UINT32 d)
+            : m_map(m), m_updater(u), m_diff(d)
+        {
+        }
+
+        virtual int call()
+        {
+            uint32 diff = getMSTime();
+            m_map.Update (m_diff);
+            diff = getMSTime() - diff;
+            if (diff > 150)
+                sLog->outDiff("Map diff: %u. ID %i Players online: %u.", diff, m_map.GetId(), sWorld->GetActiveSessionCount());
+            m_updater.update_finished();
+            return 0;
+        }
+};
+
+MapUpdater::MapUpdater():
+m_executor(), m_mutex(), m_condition(m_mutex), pending_requests(0)
+{
 }
 
-void MapUpdater::deactivate()
+MapUpdater::~MapUpdater()
 {
-    _cancelationToken = true;
+    deactivate();
+}
 
+int MapUpdater::activate(size_t num_threads)
+{
+    return m_executor.start((int)num_threads, new WDBThreadStartReq1, new WDBThreadEndReq1);
+}
+
+int MapUpdater::deactivate()
+{
     wait();
 
-    _queue.Cancel();
+    return m_executor.deactivate();
+}
 
-    for (auto &thread : _workerThreads)
+int MapUpdater::wait()
+{
+    TRINITY_GUARD(ACE_Thread_Mutex, m_mutex);
+
+    while (pending_requests > 0)
+        m_condition.wait();
+
+    return 0;
+}
+
+int MapUpdater::schedule_update(Map& map, ACE_UINT32 diff)
+{
+    TRINITY_GUARD(ACE_Thread_Mutex, m_mutex);
+
+    ++pending_requests;
+
+    if (m_executor.execute(new MapUpdateRequest(map, *this, diff)) == -1)
     {
-        thread.join();
+        ACE_DEBUG((LM_ERROR, ACE_TEXT("(%t) \n"), ACE_TEXT("Failed to schedule Map Update")));
+
+        --pending_requests;
+        return -1;
     }
-}
 
-void MapUpdater::wait()
-{
-    std::unique_lock<std::mutex> lock(_lock);
-
-    while (_pending_requests > 0)
-        _condition.wait(lock);
-
-    lock.unlock();
-}
-
-void MapUpdater::schedule_update(Map &map, uint32 diff)
-{
-    std::lock_guard<std::mutex> lock(_lock);
-    ++_pending_requests;
-    _queue.Push((MapUpdaterTask*)new MapUpdateRequest(map, *this, diff));
-}
-
-void MapUpdater::schedule_specific(MapUpdaterTask *request)
-{
-    std::lock_guard<std::mutex> lock(_lock);
-    ++_pending_requests;
-    _queue.Push(request);
+    return 0;
 }
 
 bool MapUpdater::activated()
 {
-    return _workerThreads.size() > 0;
+    return m_executor.activated();
 }
 
 void MapUpdater::update_finished()
 {
-    std::lock_guard<std::mutex> lock(_lock);
-    --_pending_requests;
-    _condition.notify_all();
-}
+    TRINITY_GUARD(ACE_Thread_Mutex, m_mutex);
 
-void MapUpdater::WorkerThread()
-{
-    while (true)
+    if (pending_requests == 0)
     {
-        MapUpdaterTask *request = nullptr;
-
-        _queue.WaitAndPop(request);
-
-        if (_cancelationToken)
-            return;
-
-        request->call();
-        delete request;
+        ACE_ERROR((LM_ERROR, ACE_TEXT("(%t)\n"), ACE_TEXT("MapUpdater::update_finished BUG, report to devs")));
+        return;
     }
+
+    --pending_requests;
+
+    m_condition.broadcast();
 }
