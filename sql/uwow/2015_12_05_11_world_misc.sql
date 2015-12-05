06321f34b585e865e933191888f2862f4cd564f5
Yourp <zen.89-89@mail.ru>
2015-12-06 00:04:39 +0200

    Чернокнижник: Луч Бездны больше не продлевает время действия эффекта Порча.

--- sql/uwow/2015_12_05_11_world_misc.sql
+++ sql/uwow/2015_12_05_11_world_misc.sql
@@ -1,1 +1,3 @@
 INSERT INTO `spell_proc_check` (`entry`, `checkspell`, `comment`) VALUES ('145091', '112092', 'Item - Warlock T16 4P Bonus');
+
+DELETE FROM `spell_script_names` WHERE (`spell_id`='115422') AND (`ScriptName`='spell_warl_void_ray');
