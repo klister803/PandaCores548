368a1ef7156db731666ad25363ae4ca16fb717b5
Yourp <zen.89-89@mail.ru>
2015-12-09 01:44:23 +0200

    Охотник: Исправлен баг, когда у игроков под эффектом Дух стаи некоторые способности вызывали эффект Головокружение. (к примеру Удар летящего змея)

--- sql/uwow/2015_12_09_10_world_misc.sql
+++ sql/uwow/2015_12_09_10_world_misc.sql
@@ -1,2 +1,5 @@
 DELETE FROM `spell_proc_event` WHERE `entry` IN (49222);
 INSERT INTO `spell_proc_event` VALUES (49222, 0, 0, 0, 0, 0, 0, 8+32+128+512+131072, 0, 0, 0, 0, 7);
+
+DELETE FROM `spell_proc_event` WHERE `entry` IN (13159);
+INSERT INTO `spell_proc_event` VALUES (13159, 0, 0, 0, 0, 0, 0, 8+32+128+512+131072, 0, 0, 0, 0, 7);
