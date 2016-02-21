c872ebaded880010e8f1e5b8dcc1c50f1b17bbe6
Yourp <zen.89-89@mail.ru>
2016-02-22 00:15:54 +0200

    Жрец: Исправлен баг, когда эффект Призрачный облик прерывался от исцеления.

--- sql/uwow/2016_02_22_10_world_misc.sql
+++ sql/uwow/2016_02_22_10_world_misc.sql
@@ -1,2 +1,5 @@
 DELETE FROM `spell_proc_event` WHERE `entry` IN (44448);
 INSERT INTO `spell_proc_event` VALUES (44448, 4, 3, 0, 0, 0, 0, 0, 0x00000C02, 0, 0, 0, 7);
+
+DELETE FROM `spell_proc_event` WHERE `entry` IN (119032);
+INSERT INTO `spell_proc_event` VALUES (119032, 0, 0, 0, 0, 0, 0, 0x000A22A8, 0, 0, 0, 0, 7);
