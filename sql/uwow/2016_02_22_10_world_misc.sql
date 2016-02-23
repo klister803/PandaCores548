0c766c9a34e72413f715d00cde8eacef57ede5c0
Yourp <zen.89-89@mail.ru>
2016-02-23 23:11:43 +0200

    Маг: Оптимизация таланта Руна мощи

--- sql/uwow/2016_02_22_10_world_misc.sql
+++ sql/uwow/2016_02_22_10_world_misc.sql
@@ -3,3 +3,5 @@ INSERT INTO `spell_proc_event` VALUES (44448, 4, 3, 0, 0, 0, 0, 0, 0x00000C02, 0
 
 DELETE FROM `spell_proc_event` WHERE `entry` IN (119032);
 INSERT INTO `spell_proc_event` VALUES (119032, 0, 0, 0, 0, 0, 0, 0x000A22A8, 0, 0, 0, 0, 7);
+
+UPDATE `areatrigger_actions` SET `aura`='-116014' WHERE (`entry`='304') AND (`id`='0');
