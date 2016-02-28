fb1bdab1ec06e5534e09218b3bcb53d040b09493
Yourp <zen.89-89@mail.ru>
2016-02-28 21:31:04 +0200

    Предмет: Исправлен баг, когда Цзина-Кан, Доброта Чи-Цзи срабатывал от не исцеляющих способностей (к примеру Отвар ци)

--- sql/uwow/2016_02_28_10_world_misc.sql
+++ sql/uwow/2016_02_28_10_world_misc.sql
@@ -1,3 +1,6 @@
 INSERT INTO `spell_target_filter` (`spellId`, `targetId`, `count`, `comments`) VALUES ('117640', '30', '6', 'Spinning Crane Kick');
 INSERT INTO `spell_target_filter` (`spellId`, `targetId`, `option`, `param1`, `comments`) VALUES ('117640', '30', '3', '16', 'Spinning Crane Kick');
 INSERT INTO `spell_target_filter` (`spellId`, `targetId`, `option`, `comments`) VALUES ('117640', '30', '16', 'Spinning Crane Kick');
+
+DELETE FROM `spell_proc_event` WHERE `entry` IN (146199);
+INSERT INTO `spell_proc_event` VALUES (146199, 0, 0, 0, 0, 0, 0, 16384, 0, 0, 0, 0, 7);
