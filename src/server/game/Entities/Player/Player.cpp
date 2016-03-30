0e84852ccf7d8bb42c07f15da721d9eb682c5d03
Yourp <zen.89-89@mail.ru>
2016-03-31 02:32:15 +0300

    это тоже вернем

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -23889,7 +23889,7 @@ void Player::DropModCharge(SpellModifier* mod, Spell* spell)
 
     if (spell)
     {
-        if (GetGlobalCooldownMgr().HasGlobalCooldown(spell->GetSpellInfo()))
+        if (mod->op == SPELLMOD_COST && GetGlobalCooldownMgr().HasGlobalCooldown(spell->GetSpellInfo()))
             return;
 
         //sLog->outDebug(LOG_FILTER_SPELLS_AURAS, "DropModCharge spell %i, modId %i, charges %i, GetCharges %i, GetStackAmount %i, op %i", spell->GetSpellInfo()->Id, mod->spellId, mod->charges, mod->ownerAura->GetCharges(), mod->ownerAura->GetStackAmount(), mod->op);
