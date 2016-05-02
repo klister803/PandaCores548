c7fa82c31ded94f9ae445e96494d2c77376bfcc3
Alex Alex <victimsnino@gmail.com>
2016-05-02 13:11:07 +0000

    Частичный возврат

--- src/server/scripts/Custom/npc_visualweapon_mop.cpp
+++ src/server/scripts/Custom/npc_visualweapon_mop.cpp
@@ -35,7 +35,7 @@ struct VisualData
  
 VisualData vData[] =
 {
-    { 1, 0, GOSSIP_ICON_BATTLE, 3789, "Берсерк (Berserk)" , 0, "Вы действительно хотите наложить это зачарование за |TInterface/ICONS/Inv_qiraj_jewelblessed:30|t Золотая Монета? (Are you really want use it`s enchantment for 1 Gold Coint?" },
+    { 1, 0, GOSSIP_ICON_BATTLE, 3789, "Берсерк" , 0, "Вы действительно хотите наложить это зачарование за |TInterface/ICONS/Inv_qiraj_jewelblessed:30|t Золотая Монета?" },
     { 1, 0, GOSSIP_ICON_BATTLE, 3854, "Сила заклинаний" , 0, "Вы действительно хотите наложить это зачарование за |TInterface/ICONS/Inv_qiraj_jewelblessed:30|t Золотая Монета?" },
     { 1, 0, GOSSIP_ICON_BATTLE, 3273, "Смертельный лёд" , 0, "Вы действительно хотите наложить это зачарование за |TInterface/ICONS/Inv_qiraj_jewelblessed:30|t Золотая Монета?" },
     { 1, 0, GOSSIP_ICON_BATTLE, 3225, "Палач" , 0, "Вы действительно хотите наложить это зачарование за |TInterface/ICONS/Inv_qiraj_jewelblessed:30|t Золотая Монета?" },
