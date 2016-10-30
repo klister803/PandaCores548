9780ca854f4dc88602911e908993e090dd5b0457
Yourp <zen.89-89@mail.ru>
2016-10-31 01:01:56 +0200

    Охотник, Разбойник: Исправлен баг, когда пандарены охотники могли носить одноручное оружие в левой руке имея оружие дальнего боя в правой.

---
+++ sql/uwow/2016_10_31_10_world_misc.sql
@@ -0,0 +1,5 @@
+DELETE FROM `playercreateinfo_spell` WHERE (`race`='24') AND (`class`='3') AND (`Spell`='114355');
+DELETE FROM `playercreateinfo_spell` WHERE (`race`='24') AND (`class`='4') AND (`Spell`='114355');
+DELETE FROM `playercreateinfo_spell` WHERE (`race`='25') AND (`class`='3') AND (`Spell`='114355');
+DELETE FROM `playercreateinfo_spell` WHERE (`race`='25') AND (`class`='4') AND (`Spell`='114355');
+DELETE FROM `playercreateinfo_spell` WHERE (`race`='26') AND (`class`='3') AND (`Spell`='114355');
