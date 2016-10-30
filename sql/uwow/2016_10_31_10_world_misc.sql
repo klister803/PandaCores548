971ec85bc4b99604dcac9be8b841dc3952e3014e
Yourp <zen.89-89@mail.ru>
2016-10-31 01:06:05 +0200

    Монах: Переписан прок "Неутомимый преследователь". Исправлен баг, когда иногда монах снимал с себя замедление.

--- sql/uwow/2016_10_31_10_world_misc.sql
+++ sql/uwow/2016_10_31_10_world_misc.sql
@@ -3,3 +3,7 @@ DELETE FROM `playercreateinfo_spell` WHERE (`race`='24') AND (`class`='4') AND (
 DELETE FROM `playercreateinfo_spell` WHERE (`race`='25') AND (`class`='3') AND (`Spell`='114355');
 DELETE FROM `playercreateinfo_spell` WHERE (`race`='25') AND (`class`='4') AND (`Spell`='114355');
 DELETE FROM `playercreateinfo_spell` WHERE (`race`='26') AND (`class`='3') AND (`Spell`='114355');
+
+
+DELETE FROM `spell_proc_event` WHERE `entry` IN (124489);
+INSERT INTO `spell_proc_event` VALUES (124489, 0, 53, 64+128, 0, 0, 0, 1024+4096, 0, 0, 100, 0, 7);
