6ed625d415480d8de08053d922919bccc9cd0f0f
Yourp <zen.89-89@mail.ru>
2016-11-22 14:02:08 +0200

    Охотник: Теперь способность Притвориться мертвым снимает эффект Камуфляж.

--- sql/uwow/2016_11_11_10_world_misc.sql
+++ sql/uwow/2016_11_11_10_world_misc.sql
@@ -1,2 +1,4 @@
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('57755', 'spell_warr_heroic_throw');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('134967', 'spell_warr_heroic_throw_glyph');
+
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `caster`, `target`, `comment`) VALUES ('5384', '-51755', '10', '10', 'Camouflage Remove');
