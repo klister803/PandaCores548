b70aacc7eb28120875913885871114f029f833fc
Yourp <zen.89-89@mail.ru>
2016-11-22 14:09:32 +0200

    Охотник: Теперь способность Орлиный глаз снимает эффект Камуфляж.

--- sql/uwow/2016_11_11_10_world_misc.sql
+++ sql/uwow/2016_11_11_10_world_misc.sql
@@ -3,3 +3,4 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('134967', 's
 
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `caster`, `target`, `comment`) VALUES ('5384', '-51755', '10', '10', 'Camouflage Remove');
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `caster`, `comment`) VALUES ('1462', '-51755', '10', 'Camouflage Remove');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `caster`, `comment`) VALUES ('6197', '-51755', '10', 'Camouflage Remove');
