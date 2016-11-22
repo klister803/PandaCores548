7a22e9e450708d1c5b0a6e5f100df08548d598dc
Yourp <zen.89-89@mail.ru>
2016-11-22 14:07:14 +0200

    Охотник: ТЕперь способность Знание зверя снимает эффект Камуфляж.

--- sql/uwow/2016_11_11_10_world_misc.sql
+++ sql/uwow/2016_11_11_10_world_misc.sql
@@ -2,3 +2,4 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('57755', 'sp
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('134967', 'spell_warr_heroic_throw_glyph');
 
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `caster`, `target`, `comment`) VALUES ('5384', '-51755', '10', '10', 'Camouflage Remove');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `caster`, `comment`) VALUES ('1462', '-51755', '10', 'Camouflage Remove');
