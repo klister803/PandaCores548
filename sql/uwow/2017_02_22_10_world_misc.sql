16bb951f57f13f434425ac960fad53400079b644
Yourp <zen.89-89@mail.ru>
2017-02-25 01:14:04 +0200

    typo

--- sql/uwow/2017_02_22_10_world_misc.sql
+++ sql/uwow/2017_02_22_10_world_misc.sql
@@ -1,4 +1,4 @@
-UPDATE `creature_template` SET `faction`='0' WHERE (`entry`='64297');
+UPDATE `creature_template` SET `faction`='35' WHERE (`entry`='64297');
 
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('114168', 'spell_warl_dark_apotheosis');
 
