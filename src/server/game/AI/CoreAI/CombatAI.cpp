28384782c7d0718e3f752ece3fc81639b41a7078
Yourp <zen.89-89@mail.ru>
2017-02-24 22:41:42 +0200

    Общее: Исправлена ошибка, когда некоторые прислужники не переставали кастовать, если им прирывали заклинание. Так же теперь они будут следовать за целью после прерывания каста, пока сново не смогут кастовать.

--- src/server/game/AI/CoreAI/CombatAI.cpp
+++ src/server/game/AI/CoreAI/CombatAI.cpp
@@ -175,8 +175,8 @@ void CasterAI::UpdateAI(uint32 diff)
     if (me->HasUnitState(UNIT_STATE_CASTING))
         return;
 
-    if (events.GetEvent())
-        if (!me->IsWithinLOSInMap(me->getVictim()))
+    if (uint32 spell_Id = events.GetEvent())
+        if (!me->IsWithinLOSInMap(me->getVictim()) || me->HasSpellCooldown(spell_Id))
             if (MotionMaster* mMaster = me->GetMotionMaster())
             {
                 mMaster->MoveChase(me->getVictim(), 0.001f);
