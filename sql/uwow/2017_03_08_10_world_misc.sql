65c8a5da458b0f9cba9273732f4db402a0c855d2
Yourp <zen.89-89@mail.ru>
2017-03-09 21:04:45 +0200

    Шаман: Переписана Искусность: волнение стихий: Теперь эффект будет срабатывать после того, как цель получила урон.  Теперь эффект может сработать от любой цели, которой цепная молния нанесла урон. Исправлен баг, когда в подсчете шанса срабатывания не учитывалась часть числа после запятой.

--- sql/uwow/2017_03_08_10_world_misc.sql
+++ sql/uwow/2017_03_08_10_world_misc.sql
@@ -1,1 +1,14 @@
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `hastalent`, `actiontype`, `comment`) VALUES ('115043', '134732', '-134732', '5', 'Battle Fatigue');
+
+DELETE FROM `spell_script_names` WHERE (`spell_id`='51505') AND (`ScriptName`='spell_mastery_elemental_overload');
+DELETE FROM `spell_script_names` WHERE (`spell_id`='117014') AND (`ScriptName`='spell_mastery_elemental_overload');
+DELETE FROM `spell_script_names` WHERE (`spell_id`='403') AND (`ScriptName`='spell_mastery_elemental_overload');
+DELETE FROM `spell_script_names` WHERE (`spell_id`='421') AND (`ScriptName`='spell_mastery_elemental_overload');
+
+DELETE FROM `spell_proc_event` WHERE `entry` IN (77222);
+INSERT INTO `spell_proc_event` VALUES (77222, 0, 0, 1+2, 4096, 0, 4, 0, 0, 0, 100, 0, 7);
+
+INSERT INTO `spell_trigger` (`spell_id`, `spell_trigger`, `option`, `effectmask`, `chance`, `check_spell_id`, `comment`) VALUES ('77222', '77451', '20', '1', '101', '51505', 'MasteryElementalOverload');
+INSERT INTO `spell_trigger` (`spell_id`, `spell_trigger`, `option`, `effectmask`, `chance`, `check_spell_id`, `comment`) VALUES ('77222', '45284', '20', '1', '101', '403', 'MasteryElementalOverload');
+INSERT INTO `spell_trigger` (`spell_id`, `spell_trigger`, `option`, `effectmask`, `chance`, `check_spell_id`, `comment`) VALUES ('77222', '45297', '20', '1', '101', '421', 'MasteryElementalOverload');
+INSERT INTO `spell_trigger` (`spell_id`, `spell_trigger`, `option`, `effectmask`, `chance`, `check_spell_id`, `comment`) VALUES ('77222', '120588', '20', '1', '101', '117014', 'MasteryElementalOverload');
