7b6cae4fd2b34392531c08a19f6160e889ff7675
Rubaka <blackmanos@yandex.ru>
2017-03-11 10:21:37 +0300

    откат

--- src/server/game/Entities/Object/Object.cpp
+++ src/server/game/Entities/Object/Object.cpp
@@ -284,10 +284,10 @@ void Object::BuildCreateUpdateBlockForPlayer(UpdateData* data, Player* target) c
         }
     }
 
-    if (!(flags & UPDATEFLAG_LIVING))
-        if (WorldObject const* worldObject = dynamic_cast<WorldObject const*>(this))
-            if (!worldObject->m_movementInfo.transportGUID.IsEmpty())
-                flags |= UPDATEFLAG_GO_TRANSPORT_POSITION;
+    // if (!(flags & UPDATEFLAG_LIVING))
+        // if (WorldObject const* worldObject = dynamic_cast<WorldObject const*>(this))
+            // if (!worldObject->m_movementInfo.transportGUID.IsEmpty())
+                // flags |= UPDATEFLAG_GO_TRANSPORT_POSITION;
 
     if (ToUnit() && ToUnit()->getVictim())
         flags |= UPDATEFLAG_HAS_TARGET;
