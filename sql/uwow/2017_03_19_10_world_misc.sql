d88e3b478a940d0c861acb9006b9b16e97ee1253
Yourp <zen.89-89@mail.ru>
2017-03-24 01:00:15 +0200

    Охотник: Теперь Символ отражающих клинков так же работает с талантом Крадущийся тигр, затаившаяся химера

--- sql/uwow/2017_03_19_10_world_misc.sql
+++ sql/uwow/2017_03_19_10_world_misc.sql
@@ -4,3 +4,5 @@ UPDATE `creature_template` SET `flags_extra`='36' WHERE (`entry`='71790');
 UPDATE `creature_template` SET `flags_extra`='36' WHERE (`entry`='71694');
 UPDATE `creature_template` SET `flags_extra`='36' WHERE (`entry`='71751');
 UPDATE `creature_template` SET `flags_extra`='36' WHERE (`entry`='71606');
+
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES ('148467', '110618', '2', 'Deterrence');
