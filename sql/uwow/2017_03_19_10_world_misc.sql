506867e3bd256e071a00efb9527352208f3181c2
Yourp <zen.89-89@mail.ru>
2017-03-23 20:59:33 +0200

    НИП: Теперь Разобранные ползучие мины, Отключенный электромагнит, Отключенная лазерная турель и Отключенная ракетная турель больше не будут паррирывать или уклоняться от атак.

--- sql/uwow/2017_03_19_10_world_misc.sql
+++ sql/uwow/2017_03_19_10_world_misc.sql
@@ -1,1 +1,6 @@
 UPDATE `spell_proc_event` SET `Cooldown`='0' WHERE (`entry`='108558') AND (`effectmask`='7');
+
+UPDATE `creature_template` SET `flags_extra`='36' WHERE (`entry`='71790');
+UPDATE `creature_template` SET `flags_extra`='36' WHERE (`entry`='71694');
+UPDATE `creature_template` SET `flags_extra`='36' WHERE (`entry`='71751');
+UPDATE `creature_template` SET `flags_extra`='36' WHERE (`entry`='71606');
