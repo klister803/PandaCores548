3dff219d119b42edaf0880dfa4f9deb54f6b0eb9
Yourp <zen.89-89@mail.ru>
2017-04-27 03:50:04 +0300

    Общее: Исправлена проблема с фазеровкой возле Ловчий Блейк в стартовой локации воргенов

--- sql/uwow/2017_04_25_10_world_misc.sql
+++ sql/uwow/2017_04_25_10_world_misc.sql
@@ -1,1 +1,4 @@
 UPDATE `pet_stats` SET `type`='2' WHERE (`entry`='54983');
+
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `autocast`, `quest_start_status`) VALUES ('59073', '4714', '14091', '1', '74');
+INSERT INTO `phase_definitions` (`zoneId`, `entry`, `phasemask`, `terrainswapmap`, `comment`) VALUES ('4714', '3', '1', '638', 'Worgen Start loc');
