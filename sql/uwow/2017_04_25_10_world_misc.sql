99892ccf522ac65429f0245fd25191f03c9084f3
Yourp <zen.89-89@mail.ru>
2017-04-29 14:22:35 +0300

    Общее: Исправлена проблема с исчезновением НИП, если сделать релог после выполненого квеста "Последняя капля человечности"

--- sql/uwow/2017_04_25_10_world_misc.sql
+++ sql/uwow/2017_04_25_10_world_misc.sql
@@ -2,3 +2,6 @@ UPDATE `pet_stats` SET `type`='2' WHERE (`entry`='54983');
 
 INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `autocast`, `quest_start_status`) VALUES ('59073', '4714', '14091', '1', '74');
 INSERT INTO `phase_definitions` (`zoneId`, `entry`, `phasemask`, `terrainswapmap`, `comment`) VALUES ('4714', '3', '1', '638', 'Worgen Start loc');
+
+INSERT INTO `spell_area` (`spell`, `area`, `quest_end`, `autocast`, `quest_start_status`) VALUES ('68481', '4714', '14375', '1', '74');
+INSERT INTO `spell_area` (`spell`, `area`, `quest_end`, `autocast`, `quest_start_status`) VALUES ('68481', '4786', '14375', '1', '74');
