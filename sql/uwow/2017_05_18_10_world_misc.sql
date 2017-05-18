6d2a1af6163c77025aab914d393f1bb080962ab7
Yourp <zen.89-89@mail.ru>
2017-05-18 17:38:03 +0300

    Друид: Теперь при получении урона древнями, призванными друидами со специализацией Страж, друид будет накапливать Отмщение

--- sql/uwow/2017_05_18_10_world_misc.sql
+++ sql/uwow/2017_05_18_10_world_misc.sql
@@ -1,1 +1,4 @@
 UPDATE `spell_trigger` SET `targetaura`='1' WHERE (`spell_id`='120267') AND (`spell_trigger`='132365') AND (`option`='24') AND (`effectmask`='7') AND (`aura`='115069') AND (`check_spell_id`='0');
+
+INSERT INTO `spell_pet_auras` (`petEntry`, `spellId`, `option`, `target`, `comment`) VALUES ('54985', '84840', '5', '2', 'Сила Природы - Отмщение');
+UPDATE `spell_trigger` SET `targetaura`='1' WHERE (`spell_id`='84840') AND (`spell_trigger`='132365') AND (`option`='24') AND (`effectmask`='7') AND (`aura`='5487') AND (`check_spell_id`='0');
