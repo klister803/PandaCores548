98e53701c7575f0ecba957cef794256ea364ebb1
Keisixoxo <boomslayer@list.ru>
2017-08-09 10:56:23 +0000

    мелкая корректировка по f3a91c7

--- sql/uwow/2017_05_18_10_world_misc.sql
+++ sql/uwow/2017_05_18_10_world_misc.sql
@@ -4,3 +4,4 @@ INSERT INTO `spell_pet_auras` (`petEntry`, `spellId`, `option`, `target`, `comme
 UPDATE `spell_trigger` SET `targetaura`='1' WHERE (`spell_id`='84840') AND (`spell_trigger`='132365') AND (`option`='24') AND (`effectmask`='7') AND (`aura`='5487') AND (`check_spell_id`='0');
 
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `hastype`, `hitmask`, `comment`) VALUES ('5246', '-3', '9', '1', 'Intimidating Shout');
+UPDATE `spell_linked_spell` SET `actiontype`='2' WHERE `spell_trigger`='5246' AND `spell_effect`='-3';
