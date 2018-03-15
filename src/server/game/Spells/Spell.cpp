87f7b4f709a4539bdba44f16e0aab0b368966a9a
Sanmay <Sanmay666@gmail.com>
2018-03-15 21:38:57 +0600

    Classes/Mage: fix usable Cold Snap in Ice Block
    Signed-off-by: Sanmay <Sanmay666@gmail.com>

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -7995,7 +7995,7 @@ SpellCastResult Spell::CheckCasterAuras() const
             Unit::AuraEffectList const& stunAuras = m_caster->GetAuraEffectsByType(SPELL_AURA_MOD_STUN);
             for (Unit::AuraEffectList::const_iterator i = stunAuras.begin(); i != stunAuras.end(); ++i)
             {
-                if (!((*i)->GetSpellInfo()->GetAllEffectsMechanicMask() & (1<<MECHANIC_STUN)))
+                if (!((*i)->GetSpellInfo()->GetAllEffectsMechanicMask() & (1<<MECHANIC_STUN)) && !(*i)->GetSpellInfo()->GetAllEffectsMechanicMask() & (1 << MECHANIC_MAGICAL_IMMUNITY))
                 {
                     foundNotStun = true;
                     break;
