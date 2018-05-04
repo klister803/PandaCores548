7518cd5e704b16507f9de5959b484de728f666e2
Victimsnino <victimsnino@gmail.com>
2018-05-04 15:20:03 +0300

    добил

--- src/server/game/Entities/Object/Object.h
+++ src/server/game/Entities/Object/Object.h
@@ -738,7 +738,7 @@ struct DigSiteInfo
 class WorldLocation : public Position
 {
     public:
-        explicit WorldLocation(uint32 _mapid = MAPID_INVALID, float _x = 0, float _y = 0, float _z = 0, float _o = 0)
+        WorldLocation(uint32 _mapid = MAPID_INVALID, float _x = 0, float _y = 0, float _z = 0, float _o = 0)
             : m_mapId(_mapid) { Relocate(_x, _y, _z, _o); }
         WorldLocation(const WorldLocation &loc) { WorldRelocate(loc); }
 
